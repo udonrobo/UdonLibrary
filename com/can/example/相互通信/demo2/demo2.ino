49ab601af9353ac3bf561df7dec486064b8eeb21
CaseyNelson314 <caseynelson314@gmail.com>
2022-10-30 00:54:09 +0900

    相互通信例を追加

---
+++ com/can/example/相互通信/demo2/demo2.ino
@@ -0,0 +1,17 @@
+#include "CanWriter.h"
+#include "CanReader.h"
+
+CanWriter<5>  writer(2);
+CanReader<10> reader(1);
+
+void setup() {
+}
+
+void loop() {
+	reader.show('\n');
+	
+	writer = millis() / 100;
+	writer.update();
+	
+	delay(1);
+}
