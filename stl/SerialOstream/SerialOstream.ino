d08c996c16c07daf9baf68eb2a370e38de536e7b
CaseyNelson314 <caseynelson314@gmail.com>
2022-12-11 00:32:03 +0900

    シリアル通信でstreamを使えるような関数追加

---
+++ stl/SerialOstream/SerialOstream.ino
@@ -0,0 +1,36 @@
+#include "SerialStream.h"
+
+#include <ostream>
+#include <sstream>
+
+class Vec2 {
+		double x;
+		double y;
+	public:
+		Vec2()
+			: x()
+			, y()
+		{}
+		Vec2(double x, double y)
+			: x(x)
+			, y(y)
+		{}
+		friend std::ostream& operator<<(std::ostream& os, const Vec2& r) {
+			return os << r.x << r.y;
+		}
+};
+
+void setup() {
+	Serial.begin(115200);
+}
+
+void loop() {
+	Vec2 vec(100, 200);
+
+	std::stringstream ss;
+	ss << vec << '\n';
+	
+	SerialStream::print(ss);
+
+	delay(10);
+}
