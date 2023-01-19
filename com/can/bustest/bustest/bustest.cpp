8a1b25ccd32acb202d7fddd038f3149aae57dc3b
CaseyNelson314 <caseynelson314@gmail.com>
2023-01-19 23:20:45 +0900

    ムーブコンストラクタ呼び出し時にメモリ不正アクセスがおきる問題修正

--- com/can/bustest/bustest/bustest.cpp
+++ com/can/bustest/bustest/bustest.cpp
@@ -5,20 +5,17 @@ template<class MessageTy>
 class CanReader
 {
 
+	uint32_t id;
 	uint8_t buffer[sizeof(MessageTy)];
+	uint32_t lastTransmissionMs;
 
 public:
 
-	template<class Bus>
-	CanReader(Bus& bus)
-		: buffer()
-	{
-		bus.joinReader(&CanReader::call, this);
-	}
-
-	void call() {
-
-	}
+	CanReader()
+		: id()
+		, buffer()
+		, lastTransmissionMs()
+	{}
 
 	void show() {
 		for (auto&& it : buffer) {
@@ -27,36 +24,77 @@ public:
 		std::cout << std::endl;
 	}
 
+
+	struct Handler {
+		uint32_t id;
+		uint8_t* buffer;
+		size_t size;
+		void* _this;
+	};
+
+	Handler getHandler() {
+		return {
+			id,
+			buffer,
+			sizeof buffer,
+			this
+		};
+	}
+
 };
 
-#include <functional>
+
 #include <vector>
 
 template<int Bus>
 class CanBusTeensy {
 
-	std::vector<std::function<void()>> readers;
+	struct Node {
+		uint16_t id;
+		uint8_t* buffer;
+		size_t size;
+		void* _this;
+	};
+	std::vector<Node> readers;
+	std::vector<Node> writers;
 
 public:
 
 	template<class MessageTy>
-	void joinReader(void (CanReader<MessageTy>::*f)(), CanReader<MessageTy>* p)
-	{
-		readers.push_back(
-			[&] {
-				if (p)
-				{
-					p->f();
-				}
+	void join(CanReader<MessageTy>& r) {
+		for (auto&& it : readers) {
+			if (&r == it._this) {
+				return;
 			}
-		);
+		}
+		auto&& hReader = r.getHandler();
+		readers.push_back({
+			static_cast<uint16_t>(hReader.id),
+			hReader.buffer,
+			hReader.size,
+			hReader._this
+			});
 	}
-
+	template<class MessageTy>
+	void detach(const CanReader<MessageTy>& r)
+	{
+		for (auto&& it : readers)
+		{
+			if (it._this == &r)
+			{
+				readers.erase(it);
+				break;
+			}
+		}
+	}
 
 	void update() {
 		for (auto&& it : readers)
 		{
-			it();
+			for (size_t i = 0; i < it.size; i++)
+			{
+				it.buffer[i] = 125;
+			}
 		}
 	}
 
@@ -66,9 +104,11 @@ public:
 int main()
 {
 	CanBusTeensy<0> bus;
-	CanReader<int> reader(bus);
+	CanReader<int> reader;
+	CanReader<uint8_t> reader1;
 
+	bus.join(reader);
+	bus.join(reader1);
 
 	bus.update();
-
 }
