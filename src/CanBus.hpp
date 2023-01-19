dd3d297d437688723a9979ebafdf2de7a4573937
CaseyNelson314 <caseynelson314@gmail.com>
2023-01-19 23:21:15 +0900

    assembly

--- src/CanBus.hpp
+++ src/CanBus.hpp
@@ -5,10 +5,7 @@
 
 #pragma once
 
-//     vvv teensy3.2 vvv        vvv teensy3.5 vvv         vvv teensy3.6 vvv       vvv teensy4.0/4.1 vvv
-#if defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__)
-#	include "CanBusTeensy.hpp"
-#endif
+#include "CanBusTeensy.hpp"
 
 #if 0 //defined(ARDUINO_RASPBERRY_PI_PICO)
 #	include "CanBusPico.hpp"
