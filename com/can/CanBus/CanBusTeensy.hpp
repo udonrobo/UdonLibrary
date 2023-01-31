efa55f50321c001409c36d922a6560ec9da6c7af
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2023-02-01 00:53:48 +0900

    リファクタリング

--- com/can/CanBus/CanBusTeensy.hpp
+++ com/can/CanBus/CanBusTeensy.hpp
@@ -1,7 +1,7 @@
 /// @file   CanBusTeensy.hpp
 /// @date   2023/01/13
 /// @brief  FlexCAN_T4ライブラリを用いたCANバス管理クラス
-/// @flow   [CPU] <--CAN[TX/RX]--> [CANトランシーバ] <--CAN[H/L]--> [BUS]
+/// @remark [CPU] <--CAN[TX/RX]--> [CANトランシーバ] <--CAN[H/L]--> [BUS]
 /// @author 大河 祐介
 
 #pragma once
@@ -15,7 +15,7 @@
 
 #	include "list.hpp"  // udon::std::list
 
-#	include "CanHandle.hpp"
+#	include "CanInfo.hpp"
 #	include "CanBusInterface.hpp"
 
 
@@ -24,15 +24,15 @@ template<CAN_DEV_TABLE Bus>
 class CanBusTeensy : public CanBusInterface
 {
 
-		static CanBusTeensy* self;
-
 		FlexCAN_T4<Bus, RX_SIZE_256, TX_SIZE_256> bus;
 		IntervalTimer                             isr;
 
-		using DataLine = udon::std::list<CanNodeHandle*>;
-		DataLine          tx    ;
-		DataLine          rx    ;
-		CanBusErrorHandle hError;
+		using DataLine = udon::std::list<CanNodeInfo*>;
+		DataLine        tx   ;
+		DataLine        rx   ;
+		CanBusErrorInfo error;
+
+		static CanBusTeensy* self;
 
 	public:
 
@@ -40,7 +40,7 @@ class CanBusTeensy : public CanBusInterface
 			: bus()
 			, tx()
 			, rx()
-			, hError()
+			, error()
 		{
 			self = this;
 		}
@@ -50,7 +50,7 @@ class CanBusTeensy : public CanBusInterface
 			end();
 		}
 
-		/// @brief 通信を開始
+		/// @brief 通信開始
 		/// @param {baudrate} 通信レート
 		void begin(const uint32_t baudrate = 1000000)
 		{
@@ -63,42 +63,7 @@ class CanBusTeensy : public CanBusInterface
 			}
 			if (rx.size())
 			{
-				bus.onReceive([](const CAN_message_t& msg) {
-					const auto now = micros();
-					const auto event = [&msg, &now](CanNodeHandle * hNode)
-					{
-						// 先頭1バイト : パケット番号
-						const uint8_t index = msg.buf[0];
-
-						// 8バイト受信データをバイト列にデコード
-						for (uint8_t i = 0; i < 7; i++)
-						{
-							const uint8_t bufIndex = i + index * 7;
-							if (bufIndex < hNode->length)
-								hNode->buffer[bufIndex] = msg.buf[i + 1];
-							else
-								break;
-						}
-						hNode->timestampUs = now;
-					};
-					for (auto && it : self->rx) {
-						if (msg.id == it->id)
-						{
-							event(it);
-						}
-					}
-
-					CAN_error_t error;
-					self->bus.error(error, false);
-					self->hError = {
-						error.TX_ERR_COUNTER,
-						error.RX_ERR_COUNTER,
-						now
-					};
-					
-				});
-
-				// 受信割り込み開始
+				bus.onReceive(eventRX);
 				isr.begin(
 				    [] { self->bus.events(); },
 				    100
@@ -106,6 +71,7 @@ class CanBusTeensy : public CanBusInterface
 			}
 		}
 
+		/// @brief 通信終了
 		void end()
 		{
 			isr.end();
@@ -113,95 +79,133 @@ class CanBusTeensy : public CanBusInterface
 			bus.disableFIFOInterrupt();
 		}
 
-		/// @brief バスを更新
+		/// @brief バス更新
 		/// @param {writeIntervalUs} 送信間隔
 		void update(uint32_t writeIntervalUs = 5000)
 		{
-			const auto now = micros();
-			if (tx.size() && now - hError.timestampUs >= writeIntervalUs)
+			if (tx.size() && micros() - error.timestampUs >= writeIntervalUs)
 			{
-				const auto event = [&now](CanNodeHandle * hNode) {
-					// 一度に8バイトしか送れないため、パケットに分割し送信
-					for (size_t index = 0; index < ceil(hNode->length / 7.0); index++)
-					{
-						CAN_message_t msg;
-
-						// 先頭1バイト : パケット番号
-						msg.id = hNode->id;
-						msg.buf[0] = index;
-
-						// バイト列を8バイト受信データにエンコード
-						for (uint8_t i = 0; i < 7; i++)
-						{
-							const uint8_t bufIndex = i + index * 7;
-
-							if (bufIndex < hNode->length)
-								msg.buf[i + 1] = hNode->buffer[bufIndex];
-							else
-								break;
-						}
-						// バスに送信
-						while (not self->bus.write(msg));
-					}
-					hNode->timestampUs = now;
-				};
-
-				for (auto && it : self->tx) {
-					event(it);
-				}
-
-				CAN_error_t error;
-				bus.error(error, false);
-				hError = {
-					error.TX_ERR_COUNTER,
-					error.RX_ERR_COUNTER,
-					now
-				};
-
+				eventTX();
 			}
 		}
 
-		CanBusErrorHandle getErrorHandle() const {
-			return hError;
+		/// @brief エラー情報取得
+		/// @return CanBusErrorInfo構造体インスタンス
+		CanBusErrorInfo getErrorInfo() const
+		{
+			return error;
 		}
 
-		void joinTX(CanNodeHandle& hNode) override
+		/// @brief TXノードをバスに参加
+		/// @param node
+		void joinTX(CanNodeInfo& node) override
 		{
-			join(tx, hNode);
-		}
-		void joinRX(CanNodeHandle& hNode) override
-		{
-			join(rx, hNode);
+			join(tx, node);
 		}
 
-		void detachRX(CanNodeHandle& hNode) override
+		/// @brief RXノードをバスに参加
+		/// @param node
+		void joinRX(CanNodeInfo& node) override
 		{
-			detach(rx, hNode);
+			join(rx, node);
 		}
-		void detachTX(CanNodeHandle& hNode) override
+
+		/// @brief RXノードをバスから解放
+		/// @param node
+		void detachRX(CanNodeInfo& node) override
 		{
-			detach(tx, hNode);
+			detach(rx, node);
+		}
+
+		/// @brief TXノードをバスから解放
+		/// @param node
+		void detachTX(CanNodeInfo& node) override
+		{
+			detach(tx, node);
 		}
 
 	private:
 
-		void join(DataLine& line, CanNodeHandle& hNode)
+		void updateErrorInfo()
+		{
+			CAN_error_t e;
+			self->bus.error(e, false);
+			self->error = {
+				e.TX_ERR_COUNTER,
+				e.RX_ERR_COUNTER,
+				micros()
+			};
+		}
+
+		void eventRX(const CAN_message_t& msg)
+		{
+			const auto event = [&msg](CanNodeInfo * node)
+			{
+				const uint8_t index = msg.buf[0];   // 先頭1バイト : パケット番号
+				for (uint8_t i = 0; i < 7; i++)     // 8バイト受信データをバイト列にデコード
+				{
+					const uint8_t bufIndex = i + index * 7;
+					if (bufIndex < node->length)
+						node->buffer[bufIndex] = msg.buf[i + 1];
+					else
+						break;
+				}
+				node->timestampUs = micros();
+			};
+			for (auto && it : self->rx)
+			{
+				if (msg.id == it->id)
+				{
+					event(it);
+				}
+			}
+			updateErrorInfo();
+		}
+
+		void eventTX()
+		{
+			const auto event = [](CanNodeInfo * node) {
+				// 一度に8バイトしか送れないため、パケットに分割し送信
+				for (size_t index = 0; index < ceil(node->length / 7.0); index++)
+				{
+					CAN_message_t msg;
+					msg.id = node->id;
+					msg.buf[0] = index;  // 先頭1バイト : パケット番号
+					for (uint8_t i = 0; i < 7; i++)  // バイト列を8バイト受信データにエンコード
+					{
+						const uint8_t bufIndex = i + index * 7;
+						if (bufIndex < node->length)
+							msg.buf[i + 1] = node->buffer[bufIndex];
+						else
+							break;
+					}
+					while (not self->bus.write(msg));  // 送信
+				}
+				node->timestampUs = micros();
+			};
+			for (auto && it : self->tx) {
+				event(it);
+			}
+			updateErrorInfo();
+		}
+
+		void join(DataLine& line, CanNodeInfo& node)
 		{
 			for (auto && it : line)
 			{
-				if (it == &hNode)  // インスタンスの重複を除外する
+				if (it == &node)  // インスタンスの重複を除外する
 				{
 					return;
 				}
 			}
-			line.push_back(&hNode);
+			line.push_back(&node);
 		}
 
-		void detach(DataLine& line, CanNodeHandle& hNode)
+		void detach(DataLine& line, CanNodeInfo& node)
 		{
 			for (auto && it = line.begin(); it != line.end(); ++it)
 			{
-				if (*it == &hNode)
+				if (*it == &node)
 				{
 					line.erase(it);
 					break;
