2d2357042fc973d412cd2ab6bb32163ead10ff32
CaseyNelson <caseynelson314@gmail.com>
2023-03-23 23:56:43 +0900

    ロボットの位置を表すPosition型を作成

--- src/common/types/Position.hpp
+++ src/common/types/Position.hpp
@@ -2,8 +2,26 @@
 
 #include <common\types\Vector2D.hpp>
 
-template<class Ty>
-struct Position
+namespace udon
 {
 
-};
+	/// @brief ロボットの位置
+	/// @tparam Ty 要素の型
+	template<class Ty>
+	struct Position
+	{
+
+		/// @brief 要素の型
+		using value_type = Ty;
+
+		/// @brief 座標
+		udon::Vector2D<value_type> vector;
+
+		/// @brief 旋回角 [rad]
+		value_type turn;
+
+	};
+
+	using Pos = Position<double>;
+
+}
