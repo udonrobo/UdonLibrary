f129a21f96a9c5ce81554cdb51e148b615b3487c
HatopoPPor <77434125+HatopoPPor@users.noreply.github.com>
2023-03-26 14:13:57 +0900

    I2Cのクラスを追加

---
+++ src/udon/com/i2c/I2CMasterReader.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <arduino.h>
+#include <Wire.h>
+
+namespace udon
+{
+
+class MasterReader {
+  private:
+    int size;
+    int address;
+    byte* data;
+  public:
+    MasterReader(int Address, int Size) {
+      data = new byte[Size];
+      for (int i = 0; i < Size; i++)
+        data[i] = 0;
+      address = Address;
+      size = Size;
+    }
+
+    void Update() {
+      Wire.requestFrom(address, size);
+      while (Wire.available()) {
+        for (int i = 0; i < size; i++)
+          data[i] = Wire.read();
+      }
+    }
+
+    byte GetData(int array_num) {
+      return data[array_num - 1];
+    }
+
+    bool GetBitData(int array_num, int bit_num) {
+      return bitRead(data[array_num - 1], bit_num);
+    }
+
+    void read_show() {
+      for (int i = 0; i < size; i++) {
+        Serial.print(data[i]);
+        Serial.print("\t");
+      }
+    }
+};
+}
---
+++ src/udon/com/i2c/I2CMasterWriter.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <arduino.h>
+#include <Wire.h>
+
+namespace udon
+{
+
+class MasterWriter {
+  private:
+    int size;
+    int address;
+    byte* data;
+  public:
+    MasterWriter(int Address, int array_size) : address(Address), size(array_size) {
+      data = new byte[size];
+      for (int i = 0; i < array_size; i++)
+        data[i] = 0;
+    }
+
+    //デストラクタ
+    virtual ~MasterWriter() {
+      delete[] data;
+    }
+
+    void SetBitData(int array_num, byte bit_num, bool bit_data) {
+      bitWrite(data[array_num - 1], bit_num, bit_data);
+    }
+
+    void SetMotorData(byte port_num, int16_t pwm) {
+      SetBitData(size, port_num - 1, ((pwm < 0) ? 0 : 1)); //モーターは五つのデータ(pwm×4、方向1つ)をやりくりする。この行では５番目のデータに方向(0か1)を代入　port_num1～4　size5 方向のビットは最後に送られる(一番値が大きい)
+      SetSingleData(port_num, abs(pwm)); //port_numの数字が小さい順にpwmが送られる
+    }
+
+    void SetSingleData(int array_num, byte single_data) {
+      data[array_num - 1] = single_data;
+    }
+
+    void Update() {
+      Wire.beginTransmission(address);
+      Wire.write(data, size);
+      Wire.endTransmission();
+    }
+
+    void reset() {
+      for (int i = 0; i < size; i++)
+        data[i] = 0;
+    }
+
+    void m_reset() {
+      for (int i = 0; i < size - 1; i++)
+        data[i] = 0;
+    }
+
+    void show() {
+      for (int i = 0; i < size; i++) {
+        Serial.print(data[i]);
+        Serial.print("\t");
+      }
+    }
+};
+}
