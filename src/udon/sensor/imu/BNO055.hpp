12cf07db0b93cfc186c944c08a06aaffa0a38264
CaseyNelson <caseynelson314@gmail.com>
2023-04-09 22:26:38 +0900

    IMU管理クラス追加

---
+++ src/udon/sensor/imu/BNO055.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+
+// https://github.com/adafruit/Adafruit_Sensor.git
+// https://github.com/adafruit/Adafruit_BNO055.git
+// https://github.com/adafruit/Adafruit_BusIO.git
+
+#include <udon\types\Euler.hpp>
+
+namespace udon
+{
+
+	class BNO055
+		: Adafruit_BNO055
+	{
+
+		udon::Euler euler;
+
+	public:
+
+		BNO055(TwoWire& wire)
+			: Adafruit_BNO055(-1, 0x28, &wire)
+			, euler()
+		{
+		}
+
+		/// @brief 通信開始
+		/// @remark Wire.begin()がAdafruit_BNO055ライブラリ側で呼び出されます
+		/// @return 正常に開始できたかどうか
+		bool begin()
+		{
+			if (!Adafruit_BNO055::begin())
+			{
+				return false;
+			}
+			return true;
+		}
+
+		/// @brief 更新
+		void update()
+		{
+			const auto libEuler = Adafruit_BNO055::getVector(Adafruit_BNO055::VECTOR_EULER);
+			euler = udon::Euler {
+				udon::ToRadians(libEuler.y()),  // roll
+				udon::ToRadians(libEuler.z()),  // pitch
+				udon::ToRadians(libEuler.x()),  // yaw
+			};
+		}
+
+		/// @brief オイラー角取得
+		/// @return オイラー角
+		udon::Euler getEuler() const
+		{
+			return euler;
+		}
+
+		void show() const
+		{
+			getEuler().show();
+		}
+	};
+
+}  // namespace udon
---
+++ src/udon/sensor/imu/Imu.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+// IMUデバイス
+#include <udon\sensor\imu\BNO055.hpp>
+
+#include <udon\types\Euler.hpp>
+
+namespace udon
+{
+
+	template<class Device>
+	class Imu
+		: public Device
+	{
+
+		/// @brief 回転方向
+		udon::Euler3D<bool> direction;
+
+		/// @brief 内積値消去用オフセット
+		udon::Euler offset;
+
+	public:
+
+		/// @brief コンストラクタ
+		/// @param Device Deviceオブジェクト
+		/// @param direction 回転方向
+		Imu(
+			Device&& device,
+			udon::Euler3D<bool>&& direction = { true, true, true }
+		)
+			: Device(std::move(device))
+			, direction(direction)
+			, offset()
+		{}
+
+		/// @brief 値を消去する
+		void clear()
+		{
+			offset = Device::getEuler();
+		}
+
+		/// @brief オイラー角を取得
+		/// @return オイラー角
+		udon::Euler getEuler() const
+		{
+			return (Device::getEuler() - offset)
+					.directionRevision(direction)  // 回転方向補正
+					.normalized(-PI, PI);          // -PI~PI の間に正規化
+		}
+
+		/// @brief オイラー角をシリアルポートに出力
+		void show()
+		{
+			getEuler().show();
+		}
+
+	};
+
+}  // namespace udon
