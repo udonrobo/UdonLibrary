4ef54bd29e27df752fd01c894e1351a01b8852e7
CaseyNelson <caseynelson314@gmail.com>
2023-04-23 11:46:41 +0900

    fix

--- example/com/can/SendMessage/reader/reader.ino
+++ example/com/can/SendMessage/reader/reader.ino
@@ -1,26 +1,5 @@
 /// ボード : teensy4.0
 
-#include <CanCommon.hpp>
+#include <Udon.hpp>
 
-CanBusTeensy<CAN1> bus;
-
-// CanBusTeensy<CAN1>バスに、監視ID0として参加
-CanReader<Message::Motor> reader(bus, 0);
-
-void setup() {
-	
-	bus.begin();
-	
-}
-
-void loop() {
-
-	// 受信内容表示
-	reader.show('\n');
-
-	// バスを更新
-	bus.update();
-
-	delay(1);
-
-}
+// todo
