e55da7c06877254759e525959da14c79ef172247
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2023-05-04 09:13:54 +0900

    ServoSpeed.hppの更新

--- src/udon/actuator/Servospeed.hpp
+++ src/udon/actuator/Servospeed.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 // #include <iostream>
-#include <arduino.h>
-#include <cmath>
+#include <Arduino.h>
 
 /*
    サーボの動く速さを調整するクラス
@@ -18,82 +17,76 @@ enum class ServoChoose : uint8_t {
 };
 
 class ServoSpeed {
-  private:
-    bool status = true;
-    unsigned int loopCycle, servo_preset = 0, target_angle = 0;
-    float move_angle = 0, next_angle = 0, value = 0, speed = 0, last_angle;
+ private:
+  bool status = true;
+  unsigned int loopCycle, servo_preset = 0, target_angle = 0;
+  float move_angle = 0, next_angle = 0, value = 0, speed = 0, last_angle;
 
-  public:
-    /// @brief ServoSpeedのコンストラクタ
-    /// @param LoopCycle 周期 us
-    /// @param Choose servoの種類選択
-    /// @param Last_Angle スタート時のサーボの角度
-    ServoSpeed(unsigned int LoopCycle, ServoChoose Choose,
-               unsigned int Last_Angle = 0) {
-      loopCycle = LoopCycle / 100;
-      last_angle = next_angle = static_cast<float>(Last_Angle);
-      switch (Choose) {
-        case ServoChoose::_20kg:
-          // 20kgservo
-          servo_preset = 180;
-          break;
-        case ServoChoose::_30kg:
-          // 30kgservo
-          servo_preset = 180;
-          break;
-        case ServoChoose::_60kg:
-          // 60kgservo
-          servo_preset = 270;
-          break;
-      }
+ public:
+  /// @brief ServoSpeedのコンストラクタ
+  /// @param LoopCycle 周期 us
+  /// @param Choose servoの種類選択
+  /// @param Last_Angle スタート時のサーボの角度
+  ServoSpeed(unsigned int LoopCycle, ServoChoose Choose,
+             unsigned int Last_Angle = 0) {
+    loopCycle = LoopCycle / 100;
+    last_angle = next_angle = static_cast<float>(Last_Angle);
+    switch (Choose) {
+      case ServoChoose::_20kg:
+        // 20kgservo
+        servo_preset = 180;
+        break;
+      case ServoChoose::_30kg:
+        // 30kgservo
+        servo_preset = 180;
+        break;
+      case ServoChoose::_60kg:
+        // 60kgservo
+        servo_preset = 270;
+        break;
     }
-    /// @brief サーボの状態を更新する関数　毎周期実行する
-    void Update() {
-      /*変化量の計算は少数で行い、計算後は整数型で返す*/
-      last_angle = next_angle;  // 前周期のデータを保管しておく
-      move_angle =
+  }
+  /// @brief サーボの状態を更新する関数　毎周期実行する
+  void update() {
+    /*変化量の計算は少数で行い、計算後は整数型で返す*/
+    last_angle = next_angle;  // 前周期のデータを保管しておく
+    move_angle =
         static_cast<float>(target_angle) - static_cast<float>(last_angle);
-      if (move_angle == 0) {
-        status = true;
-      } else {
-        status = false;
-        value = constrain(speed / static_cast<float>(loopCycle),
-                          std::abs(move_angle) * -1, std::abs(move_angle)) *
-                (move_angle < 0 ? -1 : 1);
-        next_angle += value;
-        next_angle = constrain(next_angle, 0.0, static_cast<float>(servo_preset));
-      }
+    if (move_angle == 0) {
+      status = true;
+    } else {
+      status = false;
+      value = constrain(speed / static_cast<float>(loopCycle),
+                        abs(move_angle) * -1, abs(move_angle)) *
+              (move_angle < 0 ? -1 : 1);
+      next_angle += value;
+      next_angle = constrain(next_angle, 0.0, static_cast<float>(servo_preset));
     }
+  }
 
-    /// @brief サーボの次の動きをセットする
-    /// @param r 目標角度 度
-    /// @param s 稼働速度 度/s
-    void SetAngle(unsigned int r, float s) {
-      target_angle = r;
-      speed = s;
-    }
+  /// @brief サーボの次の動きをセットする
+  /// @param r 目標角度 度
+  /// @param s 稼働速度 度/s
+  void setAngle(unsigned int r, float s) {
+    target_angle = r;
+    speed = s;
+  }
 
-    /// @brief 非常停止 今周期のデータを捨てる
-    void Stop() {
-      next_angle = last_angle;
-    }
+  /// @brief 非常停止 今周期のデータを捨てる
+  void stop() { next_angle = last_angle; }
 
-    /// @brief サーボの状態を返す
-    /// @return trueのときは停止状態
-    bool GetStatus() {
-      return status;
-    }
+  /// @brief サーボの状態を返す
+  /// @return trueのときは停止状態
+  bool getStatus() const { return status; }
 
-    /// @brief 次の目標角度取得
-    /// @return 目標角度 度
-    unsigned int GetAngle() {
-      return static_cast<unsigned int>(next_angle);
-    }
+  /// @brief 次の目標角度取得
+  /// @return 目標角度 度
+  unsigned int getAngle() const {
+    return static_cast<unsigned int>(next_angle);
+  }
 
-    /// @brief 現在設定されているサーボの稼働速度を返す
-    /// @return 現在速度 度/s
-    float SpeedValue() {
-      return speed;
-    }
+  /// @brief 現在設定されているサーボの稼働速度を返す
+  /// @return 現在速度 度/s
+  float speedValue() const { return speed; }
 };
 }  // namespace udon
