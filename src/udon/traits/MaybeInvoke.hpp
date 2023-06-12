7ba9318b9bc91a619e923c29fb90eb796288a50f
CaseyNelson <caseynelson314@gmail.com>
2023-06-12 22:00:53 +0900

    引数あり版削除

--- src/udon/traits/MaybeInvoke.hpp
+++ src/udon/traits/MaybeInvoke.hpp
@@ -2,21 +2,16 @@
 
 #include <udon/traits/HasMember.hpp>
 
-/// @brief 変数に特定のメンバ関数があれば呼び出す。なければ何もしない
+/// @brief インスタンスに特定のメンバ関数があれば呼び出す。なければ何もしない
 /// @param MethodName メンバ関数名
 /// @remark コンパイル時に呼び出すかどうか判断します
+///         呼び出し関数
 #define UDON_MAYBE_INVOKE(MethodName)                                                         \
     template <typename T>                                                                     \
     auto MaybeInvoke_##MethodName(T&& instance)                                               \
         ->typename std::enable_if<udon::has_member_function_##MethodName<T>::value>::type     \
     {                                                                                         \
         instance.MethodName();                                                                \
-    }                                                                                         \
-    template <typename T, typename... Args>                                                   \
-    auto MaybeInvoke_##MethodName(T&& instance, Args&&... args)                               \
-        ->typename std::enable_if<udon::has_member_function_##MethodName<T>::value>::type     \
-    {                                                                                         \
-        instance.MethodName(std::forward<Args>(args)...);                                     \
     }                                                                                         \
     template <typename T>                                                                     \
     auto MaybeInvoke_##MethodName(T&&)                                                        \
