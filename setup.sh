dd182042e2743b3495a43bffada6b6eafa12069f
CaseyNelson <caseynelson314@gmail.com>
2023-06-16 23:11:55 +0900

    設定をスクリプトで行うよう変更

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# UdonArduinoSTL clone
+git clone https://github.com/udonrobo/UdonArduinoSTL.git
+
+# Thirdparty libraries clone
+git clone https://github.com/autowp/arduino-mcp2515.git
+git clone https://github.com/adafruit/Adafruit_BusIO.git
+git clone https://github.com/adafruit/Adafruit_Sensor.git
+git clone https://github.com/adafruit/Adafruit_BNO055.git
