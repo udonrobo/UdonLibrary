bc3e9842095ecd24f6468b1bd49e36df6b13a067
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2023-06-17 14:22:32 +0900

    Capacity関数がbit数を取得するよう変更(bool型を1bitとして扱うため)

--- src/udon/com/serialization/Capacity.hpp
+++ src/udon/com/serialization/Capacity.hpp
@@ -5,6 +5,7 @@
 #include <type_traits>
 #include <utility>
 
+#include <Udon/Math/Ceil.hpp>
 #include <udon/algorithm/CRC8.hpp>
 #include <udon/traits/HasMember.hpp>
 #include <udon/types/Float.hpp>
@@ -12,50 +13,65 @@
 namespace udon
 {
 
-    /// @brief シリアライズ後のバイト数を求める
-    /// @tparam T 整数型
+    /// @brief シリアライズ後のビット数を求める
+    /// @tparam Bool bool型
     /// @param
     /// @return
-    template <typename T>
-    inline constexpr auto Capacity(const T&) -> typename std::enable_if<std::is_integral<T>::value, size_t>::type
+    template <typename Bool>
+    inline constexpr auto Capacity(const Bool&)
+        -> typename std::enable_if<std::is_same<Bool, bool>::value, size_t>::type
     {
-        return sizeof(T);
+        return 1;
     }
 
-    /// @brief シリアライズ後のバイト数を求める
+    /// @brief シリアライズ後のビット数を求める
+    /// @tparam Integer 整数型
+    /// @param
+    /// @return
+    template <typename Integer>
+    inline constexpr auto Capacity(const Integer&)
+        -> typename std::enable_if<std::is_integral<Integer>::value && not std::is_same<Integer, bool>::value, size_t>::type
+    {
+        return sizeof(Integer) * CHAR_BIT;
+    }
+
+    /// @brief シリアライズ後のビット数を求める
     /// @tparam T 浮動小数点型
     /// @param
     /// @return
-    template <typename T>
-    inline constexpr auto Capacity(const T&) -> typename std::enable_if<std::is_floating_point<T>::value, size_t>::type
+    template <typename Floating>
+    inline constexpr auto Capacity(const Floating&)
+        -> typename std::enable_if<std::is_floating_point<Floating>::value, size_t>::type
     {
-        return sizeof(udon::float32_t);
+        return sizeof(udon::float32_t) * CHAR_BIT;
     }
 
     UDON_HAS_MEMBER_FUNCTION(capacity);           // udon::has_member_function_capacity<T>
     UDON_HAS_STATIC_MEMBER_FUNCTION(Capacity);    // udon::has_static_member_function_Capacity<T>
 
-    /// @brief シリアライズ後のバイト数を求める
+    /// @brief シリアライズ後のビット数を求める
     /// @tparam T capacity メンバ関数を持つ型
     /// @param obj
     /// @return
     template <typename T>
-    inline constexpr auto Capacity(const T& obj) -> typename std::enable_if<udon::has_member_function_capacity<T>::value, size_t>::type
+    inline constexpr auto Capacity(const T& obj)
+        -> typename std::enable_if<udon::has_member_function_capacity<T>::value, size_t>::type
     {
         return const_cast<T&>(obj).capacity();    // T::capacity() が const でないメンバ関数な場合、obj から呼び出しできないためconstを外す
     }
 
-    /// @brief シリアライズ後のバイト数を求める
+    /// @brief シリアライズ後のビット数を求める
     /// @tparam T  Capacity 静的メンバ関数を持つ型
     /// @param obj
     /// @return
     template <typename T>
-    inline constexpr auto Capacity(const T&) -> typename std::enable_if<udon::has_static_member_function_Capacity<T>::value, size_t>::type
+    inline constexpr auto Capacity(const T&)
+        -> typename std::enable_if<udon::has_static_member_function_Capacity<T>::value, size_t>::type
     {
         return T::Capacity();
     }
 
-    /// @brief シリアライズ後のバイト数を求める
+    /// @brief シリアライズ後のビット数を求める
     /// @tparam T 配列の要素の型
     /// @tparam N 配列の要素数
     /// @param obj
@@ -76,7 +92,7 @@ namespace udon
         return Capacity<typename std::remove_reference<T>::type>(arg);
     }
 
-    /// @brief シリアライズ後のバイト数を求める
+    /// @brief シリアライズ後のビット数を求める
     /// @tparam T
     /// @param arg 可変長引数
     /// @return
@@ -86,7 +102,7 @@ namespace udon
         return Capacity(arg) + Capacity(std::forward<Args>(args)...);
     }
 
-    /// @brief シリアライズ後のバイト数を求める
+    /// @brief シリアライズ後のビット数を求める
     /// @tparam T
     /// @return
     template <typename T>
@@ -102,19 +118,8 @@ namespace udon
     template <typename T>
     inline constexpr size_t CapacityWithChecksum(const T& obj)
     {
-        return Capacity(obj) + sizeof(decltype(udon::CRC8(nullptr, 0)));
-    }
-
-    /// @brief チェックサムを含めたシリアライズ後のバイト数を求める
-    /// @tparam T
-    /// @tparam ...Args
-    /// @param arg
-    /// @param ...args
-    /// @return
-    template <typename T, typename... Args>
-    inline constexpr size_t CapacityWithChecksum(const T& head, const Args&... tails)
-    {
-        return Capacity(head) + Capacity(tails...) + sizeof(decltype(udon::CRC8(nullptr, 0)));
+        return udon::Ceil(Capacity(obj) / static_cast<double>(CHAR_BIT)) +
+               udon::CRC8_SIZE;
     }
 
     /// @brief チェックサムを含めたシリアライズ後のバイト数を求める
@@ -123,7 +128,8 @@ namespace udon
     template <typename T>
     inline constexpr size_t CapacityWithChecksum()
     {
-        return Capacity(T{}) + sizeof(decltype(udon::CRC8(nullptr, 0)));
+        return udon::Ceil(Capacity(T{}) / static_cast<double>(CHAR_BIT)) +
+               udon::CRC8_SIZE;
     }
 
 }    // namespace udon
