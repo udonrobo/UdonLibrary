1baa8e62d24b171834908ae19033edf5497dcee7
CaseyNelson <caseynelson314@gmail.com>
2023-06-20 23:06:57 +0900

    I2cのサンプルスケッチ追加

---
+++ example/com/i2c/I2cSlaveWriter/I2cSlaveWriter.ino
@@ -0,0 +1,73 @@
+//-----------------------------------------------
+//
+//	UdonLibrary
+//
+//	Copyright (c) 2022-2023 Okawa Yusuke
+//	Copyright (c) 2022-2023 udonrobo
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+//
+//  I2c スレーブ側送信クラス スケッチ例
+//
+//-----------------------------------------------
+
+#ifdef ARDUINO
+
+#    include <Udon.hpp>
+
+// I2C バス管理クラス
+udon::I2cBus bus{ Wire };
+
+// I2C スレーブ側送信クラス
+udon::I2cSlaveWriter<udon::Vec2> writer{ bus };
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // 自身のアドレスを設定し、通信を開始します。
+    bus.begin(6);
+
+    // 送信を開始します。(送信割り込み設定を行います)
+    writer.begin();
+}
+
+void loop()
+{
+    // 通信間隔を監視し、通信エラー時には再起動を行います。
+    bus.update();
+
+    // バスの状態を表示します。
+    bus.show();
+
+    // 送信データを設定します。送信クラスのテンプレートクラス引数で指定した型の値を設定してください。
+    writer.setMessage({ millis(), micros() });
+
+    Serial.println();
+
+    delay(10);
+}
+
+// 以下、シリアルモニタに表示される内容例
+// restart: 1 transmit[ms]: 26617
+// restart: 1 transmit[ms]: 26630
+// restart: 1 transmit[ms]: 26643
+// restart: 1 transmit[ms]: 26643
+// restart: 1 transmit[ms]: 26656
+// restart: 1 transmit[ms]: 26669
+// restart: 1 transmit[ms]: 26682
+// restart: 1 transmit[ms]: 26696
+// restart: 1 transmit[ms]: 26696
+// restart: 1 transmit[ms]: 26709
+// restart: 1 transmit[ms]: 26722
+// restart: 1 transmit[ms]: 26735
+// restart: 1 transmit[ms]: 26735
+// restart: 1 transmit[ms]: 26748
+// restart: 1 transmit[ms]: 26761
+// restart: 1 transmit[ms]: 26774
+// restart: 1 transmit[ms]: 26774
+// restart: 1 transmit[ms]: 26787
+
+#endif
