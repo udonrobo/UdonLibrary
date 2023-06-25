cc950210e0533ee205968ae800d7e85556f61f64
CaseyNelson <caseynelson314@gmail.com>
2023-06-25 15:32:06 +0900

    nooooooooooooooo!!!!!!

--- src/udon/math/Math.hpp
+++ src/udon/math/Math.hpp
@@ -27,84 +27,84 @@ namespace udon
     constexpr double DegToRad = 0.017453292519943295769236907684886;
     constexpr double RadToDeg = 57.295779513082320876798154814105;
 
-    template <class A, class B>
+    template <typename A, typename B>
     inline constexpr A
     Constrain(const A& amt, const B& low, const B& high)
     {
         return fmin(fmax(amt, low), high);
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline constexpr Ty
     ToRadians(const Ty& rhs)
     {
         return rhs * DegToRad;
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline constexpr Ty
     ToDegrees(const Ty& rhs)
     {
         return rhs * RadToDeg;
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline constexpr Ty
     Sq(const Ty& rhs)
     {
         return rhs * rhs;
     }
 
-    template <class T>
+    template <typename T>
     inline constexpr T
     Abs(const T& rhs)
     {
         return rhs < 0 ? -rhs : rhs;
     }
 
-    template <class T>
-    inline constexpr T
-    Min(const T& lhs, const T& rhs)
+    template <typename A, typename B>
+    inline constexpr auto
+    Min(const A& lhs, const B& rhs) -> decltype(lhs + rhs)
     {
         return lhs < rhs ? lhs : rhs;
     }
 
-    template <class T>
-    inline constexpr T
-    Max(const T& lhs, const T& rhs)
+    template <typename A, typename B>
+    inline constexpr auto
+    Max(const A& lhs, const B& rhs) -> decltype(lhs + rhs)
     {
         return lhs > rhs ? lhs : rhs;
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline bool
     BitRead(const Ty& rhs, uint8_t bit)
     {
         return (rhs >> bit) & 0x01;
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline void
     BitSet(Ty& rhs, uint8_t bit)
     {
         rhs |= (1UL << bit);
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline void
     BitClear(Ty& rhs, uint8_t bit)
     {
         rhs &= ~(1UL << bit);
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline void
     BitToggle(Ty& rhs, uint8_t bit)
     {
         rhs ^= (1UL << bit);
     }
 
-    template <class Ty>
+    template <typename Ty>
     inline void
     BitWrite(Ty& rhs, uint8_t bit, bool value)
     {
