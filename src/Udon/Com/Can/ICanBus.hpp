1dbf9db508e9fdf11e40bdf99baea8a0310c883a
CaseyNelson <caseynelson314@gmail.com>
2023-07-16 11:47:12 +0900

    add LICENSE

--- src/Udon/Com/Can/ICanBus.hpp
+++ src/Udon/Com/Can/ICanBus.hpp
@@ -1,3 +1,18 @@
+//-------------------------------------------------------------------
+//
+//    UdonLibrary
+//
+//    Copyright (c) 2022-2023 Okawa Yusuke
+//    Copyright (c) 2022-2023 udonrobo
+//
+//    Licensed under the MIT License.
+//
+//-------------------------------------------------------------------
+//
+//    CANバス管理クラス インターフェース
+//
+//-------------------------------------------------------------------
+
 #pragma once
 
 #include <Udon/Com/Can/CanNode.hpp>
@@ -10,15 +25,23 @@ namespace Udon
     public:
         virtual ~ICanBus() {}
 
+        /// @brief バスの有効性を取得
         virtual operator bool() const = 0;
 
+        /// @brief 送信ノードを登録
+        /// @param node
         virtual void joinTx(CanNode& node) = 0;
 
+        /// @brief 受信ノードを登録
+        /// @param node
+        /// @param onReceive 最終フレーム到達時に呼び出されるコールバック関数
+        /// @param p コールバック関数の引数に渡すポインタ (thisなどを渡す)
         virtual void joinRx(CanNode& node, void (*onReceive)(void*), void* p) = 0;
 
+        /// @brief 送信ノードを登録解除
         virtual void leaveTx(CanNode& node) = 0;
 
+        /// @brief 受信ノードを登録解除
         virtual void leaveRx(CanNode& node) = 0;
-
     };
 }    // namespace Udon
