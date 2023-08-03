568f66a59315251532f8e2755663b21fc93fe2c7
CaseyNelson <caseynelson314@gmail.com>
2023-08-04 03:03:07 +0900

    シリアライザの単体テスト書いた

--- test/UnitTest/Serialization/Serialization.cpp
+++ test/UnitTest/Serialization/Serialization.cpp
@@ -1,13 +1,49 @@
 #include <gtest/gtest.h>
+
 #include <Udon/Com/Serialization.hpp>
 
-TEST(Serialization, huga)
+struct Vec2
 {
-	EXPECT_STRNE("hello", "world");
-	EXPECT_EQ(7 * 6, 42);
+	double x, y;
+	bool operator==(const Vec2& rhs) const { return x == rhs.x && y == rhs.y; }
+	bool operator!=(const Vec2& rhs) const { return !(*this == rhs); }
+	UDON_PARSABLE(x, y);
+};
+
+TEST(Serialization, BufferSizeAfterSerialization)
+{
+	EXPECT_EQ(Udon::CapacityWithChecksum<bool    >(), 1     + 1);
+	EXPECT_EQ(Udon::CapacityWithChecksum<uint8_t >(), 1     + 1);
+	EXPECT_EQ(Udon::CapacityWithChecksum<uint16_t>(), 2     + 1);
+	EXPECT_EQ(Udon::CapacityWithChecksum<uint32_t>(), 4     + 1);
+	EXPECT_EQ(Udon::CapacityWithChecksum<uint64_t>(), 8     + 1);
+	EXPECT_EQ(Udon::CapacityWithChecksum<float   >(), 4     + 1);
+	EXPECT_EQ(Udon::CapacityWithChecksum<double  >(), 4     + 1);
+	EXPECT_EQ(Udon::CapacityWithChecksum<Vec2    >(), 4 + 4 + 1);
 }
 
-TEST(Serialization, hoge)
+TEST(Serialization, SerializeDeserialize)
 {
-	EXPECT_EQ(1, 1);
+	Vec2 v{ 1.0, 2.0 };
+
+	const auto serialized = Udon::Pack(v);
+    EXPECT_EQ(Udon::CapacityWithChecksum<Vec2>(), serialized.size());    // シリアライズ後のサイズが正しいか確認
+	EXPECT_EQ(serialized.back(), Udon::CRC8(serialized.data(), serialized.size() - 1));    // CRCが正しいか確認
+
+    const auto deserialized = Udon::Unpack<Vec2>(serialized);
+    EXPECT_TRUE(deserialized.has_value());    // デシリアライズに成功したか確認
+    EXPECT_EQ(v, deserialized.value());       // デシリアライズ後の値が正しいか確認
+}
+
+TEST(Serialization, DeserializeError)
+{
+	Vec2 v{ 1.0, 2.0 };
+	auto serialized = Udon::Pack(v);
+
+	serialized[2] = 0x00;  // CRCが不正になるように変更
+	
+	const auto deserialized = Udon::Unpack<Vec2>(serialized);
+
+	EXPECT_FALSE(deserialized.has_value());
+	EXPECT_NE(v, deserialized.value());
 }
