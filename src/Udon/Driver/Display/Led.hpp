8974a2273d73a744a08329817a81a9fd67c49f5a
CaseyNelson <caseynelson314@gmail.com>
2023-08-26 20:15:56 +0900

    LEDの点滅パターン追加

--- src/Udon/Driver/Display/Led.hpp
+++ src/Udon/Driver/Display/Led.hpp
@@ -14,24 +14,42 @@ namespace Udon
         {
         }
 
-        void begin()
+        void begin() const
         {
             pinMode(pin, OUTPUT);
         }
 
-        void on()
+        void on() const
         {
-            write(true);
+            digitalWrite(true);
         }
 
-        void off()
+        void off() const
         {
-            write(false);
+            digitalWrite(false);
         }
 
-        void write(bool value)
+        void digitalWrite(bool value) const
         {
-            digitalWrite(pin, value ? HIGH : LOW);
+            ::digitalWrite(pin, value ? HIGH : LOW);
+        }
+
+        void analogWrite(uint8_t value) const
+        {
+            ::analogWrite(pin, value);
+        }
+
+        /// @brief 時間を基に点滅
+        void flush(uint32_t intervalMs = 500, uint32_t onTimeMs = 50) const
+        {
+            digitalWrite(millis() % intervalMs < onTimeMs);
+        }
+
+        /// @brief 時間を基にsin波のように点滅
+        /// @param intervalMs 
+        void wave(uint32_t intervalMs = 500) const
+        {
+            analogWrite(abs(map(millis() % intervalMs, 0, intervalMs, 0, 510) - 255));
         }
     };
 }    // namespace Udon
