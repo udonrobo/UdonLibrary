4c41e2585e23c0fc40de00e79b0cbbc0a5e04d57
CaseyNelson <caseynelson314@gmail.com>
2023-08-30 00:31:00 +0900

    スケッチ例追加

---
+++ example/Encoder/ReadMultipleEncoderPico/ReadMultipleEncoderPico.ino
@@ -0,0 +1,47 @@
+//-------------------------------------------------------------------
+//
+//    UdonLibrary
+//
+//    Copyright (c) 2022-2023 Okawa Yusuke
+//    Copyright (c) 2022-2023 udonrobo
+//
+//    Licensed under the MIT License.
+//
+//-------------------------------------------------------------------
+//
+//    複数のエンコーダーのカウント値を取得する例 (Raspberry Pi Pico)
+//
+//-------------------------------------------------------------------
+
+#include <Udon.hpp>
+
+Udon::EncoderPico encoders[]{
+    { 0, 1 },
+    { 2, 3 },
+    { 4, 5 },
+    { 6, 7 },
+    { 8, 9 },
+    { 10, 11 },
+    { 12, 13 },
+    { 14, 15 },
+    { 22, 26 },
+};
+
+void setup()
+{
+    for (auto&& encoder : encoders)
+    {
+        encoder.begin();
+    }
+}
+
+void loop()
+{
+    for (auto&& encoder : encoders)
+    {
+        Serial.print(encoder.read());
+        Serial.print('\t');
+    }
+    Serial.println();
+    delay(10);
+}
