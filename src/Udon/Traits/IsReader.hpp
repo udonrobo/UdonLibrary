18a06307ef43d167112bd5dc03ebae46b592238c
CaseyNelson <caseynelson314@gmail.com>
2023-08-30 04:03:12 +0900

    IsReader, IsWriter の定義場所移動、void_tを用いて再実装

---
+++ src/Udon/Traits/IsReader.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Udon/Stl/EnableSTL.hpp>
+#include <type_traits>
+
+namespace Udon
+{
+
+    namespace Traits
+    {
+
+        /// @brief 送信クラスであるか判定するトレイト
+        /// @details 送信クラスの要件を満たすには、以下の条件を満たす必要がある。
+        /// - `MessageType` というメンバ型名を持つ
+        /// - `getMessage()` メンバ関数を持ち、戻り値が `MessageType`、引数が存在しない
+        template <template <typename> typename, typename = void>
+        struct IsWriter : std::false_type
+        {
+        };
+
+        template <template <typename> typename T>
+        struct IsWriter<T, std::void_t<
+                               typename std::is_same<
+                                   decltype(std::declval<T<int>>().getMessage()), typename T<int>::MessageType>::type>> : std::true_type
+        {
+        };
+
+    }    // namespace Traits
+
+}    // namespace Udon
---
+++ src/Udon/Traits/IsWriter.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Udon/Stl/EnableSTL.hpp>
+#include <type_traits>
+
+namespace Udon
+{
+
+    namespace Traits
+    {
+
+        /// @brief 送信クラスであるか判定するトレイト
+        /// @details 送信クラスの要件を満たすには、以下の条件を満たす必要がある。
+        /// - `MessageType` というメンバ型名を持つ
+        /// - `setMessage()` メンバ関数を持ち、戻り値が `void`、引数型が `const MessageType&` である
+        template <template <typename> typename, typename = void>
+        class IsWriter : std::false_type
+        {
+        };
+
+        template <template <typename> typename T>
+        class IsWriter<T, std::void_t<
+                              typename std::is_same<
+                                  decltype(std::declval<T<int>>().setMessage(std::declval<const typename T<int>::MessageType&>())), void>::type>> : std::true_type
+        {
+        };
+
+    }    // namespace Traits
+
+}    // namespace Udon
