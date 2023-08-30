862eeb7ac26524e243a8021418f16e9f86b5d003
CaseyNelson <caseynelson314@gmail.com>
2023-08-31 00:48:54 +0900

    フォーマット変更、std::enable_if出かけるとこはvoid_tから置き換え

--- src/Udon/Traits/Accessible.hpp
+++ src/Udon/Traits/Accessible.hpp
@@ -4,6 +4,7 @@
 #include <type_traits>
 
 #include "VoidT.hpp"
+#include "AlwaysFalse.hpp"
 
 namespace Udon
 {
@@ -18,25 +19,27 @@ namespace Udon
 
         /// @brief T に `template <typename Acc> void T::accessor(Acc&)` 関数が存在するか
         template <typename, typename = void>
-        struct HasMemberFunctionAccessor : std::false_type
+        struct HasMemberFunctionAccessor
+            : std::false_type
         {
         };
         template <typename T>
-        struct HasMemberFunctionAccessor<T, std::void_t<
-                                                typename std::is_same<
-                                                    decltype(std::declval<T>().accessor(std::declval<Detail::DummyAccessor&>())), void>::type>> : std::true_type
+        struct HasMemberFunctionAccessor<T, typename std::enable_if<std::is_same<
+                                                decltype(std::declval<T>().accessor(std::declval<Detail::DummyAccessor&>())), void>::value>::type>
+            : std::true_type
         {
         };
 
         /// @brief グローバル関数 `template <typename Acc, typename T> void Accessor(Acc&, T&)` が呼び出せるか
         template <typename, typename = void>
-        struct AccessorInvocable : std::false_type
+        struct AccessorInvocable
+            : std::false_type
         {
         };
         template <typename T>
-        struct AccessorInvocable<T, std::void_t<
-                                        typename std::is_same<
-                                            decltype(Accessor(std::declval<Detail::DummyAccessor&>(), std::declval<T&>())), void>::type>> : std::true_type
+        struct AccessorInvocable<T, typename std::enable_if<std::is_same<
+                                        decltype(Accessor(std::declval<Detail::DummyAccessor&>(), std::declval<T&>())), void>::value>::type>
+            : std::true_type
         {
         };
 
@@ -45,13 +48,22 @@ namespace Udon
         ///         T に `template <typename Acc> void T::accessor(Acc&)` 関数が存在する
         ///         グローバル関数 `template <typename Acc, typename T> void Accessor(Acc&, T&)` が呼び出せる
         template <typename, typename = void>
-        struct Accessible : std::false_type
+        struct Accessible
+            : std::false_type
         {
         };
         template <typename T>
         struct Accessible<T, typename std::enable_if<
-                                 HasMemberFunctionAccessor<T>::value ^ AccessorInvocable<T>::value>::type> : std::true_type
+                                 HasMemberFunctionAccessor<T>::value ^ AccessorInvocable<T>::value>::type>
+            : std::true_type
         {
+        };
+        template <typename T>
+        struct Accessible<T, typename std::enable_if<
+                                 HasMemberFunctionAccessor<T>::value && AccessorInvocable<T>::value>::type>
+            : std::true_type
+        {
+            static_assert(AlwaysFalse<T>::value, "T has both member function accessor() and global function Accessor().");    // メンバ関数とグローバル関数の両方が定義されている場合はコンパイルエラー
         };
 
         template <typename Acc, typename HasMemberFunctionAccessor, typename std::enable_if<Traits::HasMemberFunctionAccessor<HasMemberFunctionAccessor>::value, std::nullptr_t>::type* = nullptr>
--- src/Udon/Traits/Parsable.hpp
+++ src/Udon/Traits/Parsable.hpp
@@ -15,47 +15,105 @@
 
 #pragma once
 
-#include <Udon/Traits/Capacitable.hpp>
-#include <Udon/Traits/Accessible.hpp>
 #include <Udon/Com/Serialization/Capacity.hpp>
+
+#include "Capacitable.hpp"
+#include "Accessible.hpp"
+#include "AlwaysFalse.hpp"
 
 namespace Udon
 {
 
     namespace Traits
     {
+        template <typename, typename = void>
+        struct HasMemberFunctionParsable
+            : std::false_type
+        {
+        };
+        template <typename T>
+        struct HasMemberFunctionParsable<T, typename std::enable_if<std::is_same<
+                                                decltype(std::declval<T>().parsable()), bool>::value>::type>
+            : std::true_type
+        {
+        };
+
+        template <typename, typename = void>
+        struct ParsableInvocable
+            : std::false_type
+        {
+        };
+        template <typename T>
+        struct ParsableInvocable<T, typename std::enable_if<std::is_same<
+                                        decltype(Parsable(std::declval<const T&>())), bool>::value>::type>
+            : std::true_type
+        {
+        };
+
+        template <typename HasMemberFunctionParsable, typename std::enable_if<Traits::HasMemberFunctionParsable<HasMemberFunctionParsable>::value, std::nullptr_t>::type* = nullptr>
+        inline constexpr bool InvokeParsable(const HasMemberFunctionParsable& rhs)
+        {
+            return rhs.parsable();
+        }
+        template <typename ParsableInvocable, typename std::enable_if<Traits::ParsableInvocable<ParsableInvocable>::value, std::nullptr_t>::type* = nullptr>
+        inline constexpr bool InvokeParsable(const ParsableInvocable& rhs)
+        {
+            return Parsable(rhs);
+        }
+
+        template <typename T, typename = void>
+        struct MemberParsable
+            : std::false_type
+        {
+        };
+        template <typename T>
+        struct MemberParsable<T, typename std::enable_if<
+                                     HasMemberFunctionParsable<T>::value ^ ParsableInvocable<T>::value>::type>
+        {
+            static constexpr bool value = InvokeParsable(T{});
+        };
+        template <typename T>
+        struct MemberParsable<T, typename std::enable_if<
+                                     HasMemberFunctionParsable<T>::value && ParsableInvocable<T>::value>::type>
+            : std::false_type
+        {
+            static_assert(AlwaysFalse<T>::value, "T has both member function parsable() and global function Parsable().");    // メンバ関数とグローバル関数の両方が定義されている場合はコンパイルエラー
+        };
 
         /// @brief T がパース可能であるか
         template <typename, typename = void>
-        struct Parsable : std::false_type
+        struct Parsable
+            : std::false_type
         {
         };
-
         template <typename T>
-        struct Parsable<T, typename std::enable_if<std::is_arithmetic<T>::value>::type> : std::true_type    // 算術型
+        struct Parsable<T, typename std::enable_if<std::is_arithmetic<T>::value>::type>    // 算術型
+            : std::true_type
         {
         };
-
         template <typename T>
-        struct Parsable<T, typename std::enable_if<Traits::Capacitable<T>::value && Traits::Accessible<T>::value && T().parsable()>::type> : std::true_type    // ユーザー定義型かつパース可能(UDON_PARSABLE()で定義されたメンバー関数を持つ)
+        struct Parsable<T, typename std::enable_if<Capacitable<T>::value &&            // バッファサイズを返すメンバー関数を持つ
+                                                   Accessible<T>::value &&             // アクセッサを持つ
+                                                   MemberParsable<T>::value>::type>    // 各メンバがパース可能である
+            : std::true_type
         {
         };
-
         template <typename T, size_t N>
-        struct Parsable<T[N]> : Parsable<T>    // 配列
+        struct Parsable<T[N]>    // 配列
+            : Parsable<T>
         {
         };
 
         /// @brief メンバ変数がパース可能であることをコンパイル時に検証する
         template <typename Head>
-        inline constexpr bool IsMemberParsable(Head&&)
+        inline constexpr bool IsMemberParsable(const Head&)
         {
-            return Parsable<typename std::remove_reference<Head>::type>::value;
+            return Parsable<Head>::value;
         }
         template <typename Head, typename... Tails>
-        inline constexpr bool IsMemberParsable(Head&& head, Tails&&... tails)
+        inline constexpr bool IsMemberParsable(const Head& head, const Tails&... tails)
         {
-            return IsMemberParsable(std::forward<Head>(head)) && IsMemberParsable(std::forward<Tails>(tails)...);
+            return IsMemberParsable(head) && IsMemberParsable(tails...);
         }
 
     }    // namespace Traits
