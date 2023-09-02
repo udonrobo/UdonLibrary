eea94d000c807ba58149c46f01ff00f609a82225
CaseyNelson <caseynelson314@gmail.com>
2023-09-02 12:46:40 +0900

    update test

--- test/ArduinoLint/src/Driver/Actuator/MotorDrive.cpp
+++ test/ArduinoLint/src/Driver/Actuator/MotorDrive.cpp
@@ -3,14 +3,14 @@
 inline void test()
 {
     {
-        Udon::Motor2Driver motor{ 0, 0 };
+        Udon::Motor2 motor{ 0, 0 };
         motor.move(0);
         motor.stop();
         motor.show();
     }
 
     {
-        Udon::Motor3Driver motor{ 0, 0, 0 };
+        Udon::Motor3 motor{ 0, 0, 0 };
         motor.move(0);
         motor.stop();
         motor.show();
