251a8d98abbcf4cb066e18c2baca9cdbc5b9d937
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2023-09-11 13:22:37 +0900

    どこのステートマシンを使用しているかプリントする関数(ShowStateMachineInfo)追加

--- src/Udon/Driver/Pio/StateMachineAllocator.hpp
+++ src/Udon/Driver/Pio/StateMachineAllocator.hpp
@@ -10,26 +10,27 @@ namespace Udon
     namespace Pio
     {
 
-        // PIOはpio0とpio1の2つがあり、それぞれ4つのステートマシンを持つ
-        //
-        // ------- pio0 ------  ------- pio1 ------
-        // | state machine 0 |  | state machine 0 |
-        // | state machine 1 |  | state machine 1 |
-        // | state machine 2 |  | state machine 2 |
-        // | state machine 3 |  | state machine 3 |
-        // -------------------  -------------------
-
+        /// @brief ステートマシンを表す構造体
+        /// PIOはpio0とpio1の2つがあり、それぞれ4つのステートマシンを持つ
+        ///
+        /// ------- pio0 ------  ------- pio1 ------
+        /// | state machine 0 |  | state machine 0 |
+        /// | state machine 1 |  | state machine 1 |
+        /// | state machine 2 |  | state machine 2 |
+        /// | state machine 3 |  | state machine 3 |
+        /// -------------------  -------------------
         struct StateMachine
         {
-            PIO pio;      // pio0 or pio1
-            int index;    // state machine index 0~3
-            int offset;   // offset of the program
+            PIO pio;       // pio0 or pio1
+            int index;     // state machine index 0~3
+            int offset;    // offset of the program
         };
 
         /// @brief 使用可能なステートマシンを検索し割り当てる
         /// @param program ステートマシンにロードするプログラム
+        /// @remark プログラムのロード(pio_add_program)も行います
         /// @return ステートマシンの割り当てに成功した場合はステートマシンの情報を返す それ以外はnulloptを返す
-        Udon::Optional<StateMachine> AllocateStateMachine(const pio_program& program)
+        inline Udon::Optional<StateMachine> AllocateStateMachine(const pio_program& program)
         {
             PIOProgram allocator(&program);
 
@@ -41,6 +42,24 @@ namespace Udon
             }
 
             return sm;
+        }
+
+        inline void ShowStateMachineInfo(const StateMachine& sm)
+        {
+            if (sm.pio == pio0)
+            {
+                Serial.print("pio0");
+            }
+            else
+            {
+                Serial.print("pio1");
+            }
+
+            Serial.print(" sm=");
+            Serial.print(sm.index);
+
+            Serial.print(" offset=");
+            Serial.print(sm.offset);
         }
 
     }    // namespace Pio
