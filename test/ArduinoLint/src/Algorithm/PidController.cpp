66260e9bef8456798dfcc92ba3c6673375110727
CaseyNelson <caseynelson314@gmail.com>
2023-10-13 00:42:13 +0900

    PID定数を一周期のみ変更する関数を追加

--- test/ArduinoLint/src/Algorithm/PidController.cpp
+++ test/ArduinoLint/src/Algorithm/PidController.cpp
@@ -3,13 +3,30 @@
 inline void test()
 {
     Udon::PidController pid{ 0.0, 0.0, 0.0, 1000 };
+    
+    pid.update(0.0, 0.0);
+    pid.getPower();
+
+    (void)pid(0.0, 0.0);
+
+    pid.clearPower();
+
+    pid.requestParamPro(0.0);
+    pid.requestParamInt(0.0);
+    pid.requestParamDif(0.0);
+    pid.requestParam({ 0.0, 0.0, 0.0 });
+
     pid.setParamPro(0.0);
     pid.setParamInt(0.0);
     pid.setParamDif(0.0);
-    pid.getProPower();
-    pid.getIntPower();
-    pid.getDifPower();
-    pid.clearPower();
-    pid.getPower();
-    pid.update(10, 20);
+    pid.setParam({ 0.0, 0.0, 0.0 });
+
+    (void)pid.getParamPro();
+    (void)pid.getParamInt();
+    (void)pid.getParamDif();
+    (void)pid.getParam();
+
+    (void)pid.getPowerPro();
+    (void)pid.getPowerInt();
+    (void)pid.getPowerDif();
 }
