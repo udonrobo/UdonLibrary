e1be473c6e1a529af5d8c69399743f6b236f93b3
CaseyNelson <caseynelson314@gmail.com>
2023-10-28 00:41:13 +0900

    ファイル分割

---
+++ src/Udon/Com/Lora/Detail/E220.hpp
@@ -0,0 +1,374 @@
+#pragma once
+
+namespace Udon
+{
+    E220::E220(HardwareSerial& uart, uint8_t M0Pin, uint8_t M1Pin)
+        : uart(uart)
+        , M0Pin(M0Pin)
+        , M1Pin(M1Pin)
+        , AUXPin(Udon::nullopt)
+    {
+    }
+
+    E220::E220(HardwareSerial& uart, uint8_t M0Pin, uint8_t M1Pin, uint8_t AUXPin)
+        : uart(uart)
+        , M0Pin(M0Pin)
+        , M1Pin(M1Pin)
+        , AUXPin(AUXPin)
+    {
+    }
+
+    inline bool E220::begin(const uint8_t channel, const uint16_t myAddress, const uint16_t targetAddress)
+    {
+        this->channel       = channel;
+        this->targetAddress = targetAddress;
+
+        uart.begin(9600);    // パラメータセット時のビットレートは固定されている。
+
+        if (AUXPin)
+        {
+            pinMode(*AUXPin, INPUT);
+        }
+        pinMode(M0Pin, OUTPUT);
+        pinMode(M1Pin, OUTPUT);
+
+        uart.setTimeout(10);
+
+        waitUntilCommandAccept();
+        digitalWrite(M0Pin, HIGH);    // M0をHIGHに設定
+        digitalWrite(M1Pin, HIGH);    // M1をHIGHに設定
+                                      // configMode パラメータセットモード
+
+        waitUntilCommandAccept();
+        uart.write((uint8_t)0xC1);
+        uart.write((uint8_t)0x00);
+        uart.write((uint8_t)0x08);
+
+        delay(500);
+        uart.read();    // 0xC1
+        uart.read();    // 0x00
+        uart.read();    // 0x08
+
+        const uint8_t ADDH = (myAddress >> 8) & 0xFF;
+        const uint8_t ADDL = (myAddress >> 0) & 0xFF;
+
+        const uint8_t defADDH      = uart.read();    // ADDH
+        const uint8_t defADDL      = uart.read();    // ADDL
+        const uint8_t defRate      = uart.read();    // uartRate(7,6,5) & airRate(4,3,2,1,0)
+        const uint8_t defPower     = uart.read();    // subPacket(7,6) & RSSI noise(5) & notUsed(4,3,2) & SendPower(1,0)
+        const uint8_t defChannel   = uart.read();    // channel
+        const uint8_t defTransMode = uart.read();    // add RSSIByte(7) & transmittionMode(6) & notUsed(5,4,3) & worCycle(2,1,0)
+        uart.read();                                 // KeyHighByte
+        uart.read();                                 // KeyLowByte
+        uart.read();                                 // Version
+
+        const uint8_t uartRate  = defRate >> 5;
+        const uint8_t airRate   = defRate && 0b00011111;
+        const uint8_t subPacket = defPower >> 6;
+        const uint8_t RSSInoise = (defPower >> 5) && 0b00000001;
+        const uint8_t sendPower = defPower && 0b00000011;
+        const uint8_t RSSIByte  = defTransMode >> 7;
+        const uint8_t transMode = (defTransMode >> 6) && 0b00000001;
+        const uint8_t worCycle  = defTransMode && 0b00000111;
+        // 受信完了
+        // ＜"C0"とか"C3"＞＋<開始アドレス>＋＜長さ＞＋<パラメータ>
+        Serial.println("defaultParameters-----------------");
+        Serial.print("ADDH     :");
+        Serial.println(defADDH);
+        Serial.print("ADDL     :");
+        Serial.println(defADDL);
+        Serial.print("channel  :");
+        Serial.println(defChannel);
+        Serial.print("uartRate :");
+        Serial.println(uartRate);
+        Serial.print("airRate  :");
+        Serial.println(airRate);
+        Serial.print("subPacket:");
+        Serial.println(subPacket);
+        Serial.print("sendPower:");
+        Serial.println(sendPower);
+        Serial.print("RSSInoise:");
+        Serial.println(RSSInoise ? "add" : "not add");
+        Serial.print("add RSSIByte :");
+        Serial.println(RSSIByte ? "add" : "not add");
+        Serial.print("TransMode:");
+        Serial.println(transMode ? "FixedTransmissionMode" : "TransparentMode");
+        Serial.print("WorCycle:");
+        Serial.println(worCycle);
+        Serial.println("---------------------------------");
+
+        Serial.println("setParameters-----------------");
+
+        if (ADDH != defADDH)
+        {
+            waitUntilCommandAccept();
+            uart.write((uint8_t)0xC2);
+            uart.write((uint8_t)0x00);
+            uart.write((uint8_t)0x01);
+            uart.write(ADDH);
+            delay(200);
+            uart.read();    // 0xC1
+            uart.read();    // 0x00
+            uart.read();    // 0x01
+        }
+
+        if (ADDL != defADDL)
+        {
+            waitUntilCommandAccept();
+            uart.write((uint8_t)0xC2);
+            uart.write((uint8_t)0x01);
+            uart.write((uint8_t)0x01);
+            uart.write(ADDL);
+            delay(200);
+            uart.read();    // 0xC1
+            uart.read();    // 0x01
+            uart.read();    // 0x01
+        }
+
+        if (ADDH != defADDH or ADDL != defADDL)
+        {
+            Serial.print("Address :");
+            Serial.print(myAddress);
+        }
+
+        if (defRate != 0b11100010)
+        {
+            waitUntilCommandAccept();
+            uart.write((uint8_t)0xC2);
+            uart.write((uint8_t)0x02);
+            uart.write((uint8_t)0x01);
+            uart.write((uint8_t)0b11100010);
+            delay(200);
+            uart.read();    // 0xC1
+            uart.read();    // 0x02
+            uart.read();    // 0x01
+            Serial.print("Uart & AirRate:");
+            Serial.println(uart.read(), BIN);
+        }
+
+        if (defPower != 0b00000001)
+        {
+            waitUntilCommandAccept();
+            uart.write((uint8_t)0xC2);
+            uart.write((uint8_t)0x03);
+            uart.write((uint8_t)0x01);
+            uart.write((uint8_t)0b00000001);
+            delay(200);
+            uart.read();    // 0xC1
+            uart.read();    // 0x03
+            uart.read();    // 0x01
+            Serial.print("SubPacket & RSSINoise & SendPower:");
+            Serial.println(uart.read(), BIN);
+        }
+
+        if (channel != defChannel)
+        {
+            waitUntilCommandAccept();
+            uart.write((uint8_t)0xC2);
+            uart.write((uint8_t)0x04);
+            uart.write((uint8_t)0x01);
+            uart.write(channel);
+            delay(200);
+            uart.read();    // 0xC1
+            uart.read();    // 0x02
+            uart.read();    // 0x01
+            Serial.print("Channel :");
+            Serial.println(uart.read());
+        }
+
+        if (transMode != 0b01000011)
+        {
+            waitUntilCommandAccept();
+            uart.write((uint8_t)0xC2);
+            uart.write((uint8_t)0x05);
+            uart.write((uint8_t)0x01);
+            uart.write((uint8_t)0b01000011);
+            delay(200);
+            uart.read();    // 0xC1
+            uart.read();    // 0x05
+            uart.read();    // 0x01
+            Serial.print("RSSIByte & TransMode & WORCycle:");
+            Serial.println(uart.read(), BIN);
+        }
+        Serial.println("---------------------------------");
+
+        // 設定モードの終了
+        waitUntilCommandAccept();
+        digitalWrite(M0Pin, LOW);
+        digitalWrite(M1Pin, LOW);
+
+        waitUntilCommandAccept();
+        uart.end();
+        uart.begin(115200);    // 通常通信モードのレートに変更する
+        uart.setTimeout(10);
+        return true;
+    }
+
+    inline void E220::update()
+    {
+        switch (getTransmitMode())
+        {
+        case TransmitMode::Send:
+            sendUpdate();
+            break;
+        case TransmitMode::Receive:
+            receiveUpdate();
+            break;
+        case TransmitMode::TwoWay:
+            twoWayUpdate();
+            break;
+        case TransmitMode::Empty:
+            break;
+        }
+    }
+
+    inline void E220::show() const
+    {
+        if (operator bool())
+        {
+            Serial.print("E220: OK  ");
+        }
+        else
+        {
+            Serial.print("E220: NG  ");
+        }
+
+        switch (getTransmitMode())
+        {
+        case TransmitMode::Send: Serial.print("SendMode "); break;
+        case TransmitMode::Receive: Serial.print("ReceiveMode "); break;
+        case TransmitMode::TwoWay: Serial.print("TwoWayMode "); break;
+        case TransmitMode::Empty: Serial.print("Empty "); break;
+        }
+    }
+
+    /// @brief 通信モードを取得する
+    inline E220::TransmitMode E220::getTransmitMode() const
+    {
+        if (txNode)
+        {
+            if (rxNode)
+            {
+                return TransmitMode::TwoWay;
+            }
+            else
+            {
+                return TransmitMode::Send;
+            }
+        }
+        else if (rxNode)
+        {
+            return TransmitMode::Receive;
+        }
+        else
+        {
+            return TransmitMode::Empty;
+        }
+    }
+
+    inline bool E220::sendUpdate()
+    {
+        // 送信休止時間分間隔をあける
+        if (millis() - txNode->transmitMs > 50)
+        {
+            uart.write((targetAddress >> 8) & 0xFF);    // 上位アドレス
+            uart.write((targetAddress >> 0) & 0xFF);    // 下位アドレス
+            uart.write(channel);
+            Udon::BitPack(txNode->data, txNode->data + txNode->size, [this](uint8_t data)
+                          { uart.write(data); });
+            txNode->transmitMs = millis();
+            Serial.println("send");
+        }
+        else
+        {
+            // 送信間隔が短すぎる場合は送信しない
+            return false;
+        }
+        return true;
+    }
+
+    inline bool E220::receiveUpdate()
+    {
+        // FrameSize = data
+        const int FrameSize = Udon::BitPackedSize(rxNode->size);
+
+        // 受信バッファにデータがない場合は何もしない
+        if (uart.available() < FrameSize)
+        {
+            return false;
+        }
+
+        // while (uart.peek() >> 7 != 1)
+        // {
+        //     uart.read();
+        //     if (uart.available() < FrameSize)
+        //     {
+        //         return false;
+        //     }
+        // }
+
+        // データ読み込み
+        if (Udon::BitUnpack(rxNode->data, rxNode->data + rxNode->size, [this]() -> uint8_t
+                            { return uart.read(); }))
+        {
+            rxNode->transmitMs = millis();
+            return true;
+        }
+        else
+        {
+            // 先頭バイトの  MSB が 1 でない場合はデータが壊れているので読み捨て
+            while (uart.available()) uart.read();
+            return false;
+        }
+    }
+
+    inline void E220::twoWayUpdate()
+    {
+    }
+
+    inline bool E220::isTimeout(uint32_t timeoutMs) const
+    {
+        switch (getTransmitMode())
+        {
+        case TransmitMode::Send:
+            return millis() - txNode->transmitMs > timeoutMs;
+        case TransmitMode::Receive:
+            return millis() - rxNode->transmitMs > timeoutMs;
+        case TransmitMode::TwoWay:
+            return millis() - txNode->transmitMs > timeoutMs || millis() - rxNode->transmitMs > timeoutMs;
+        case TransmitMode::Empty:
+            return false;
+        default:
+            return false;
+        }
+    }
+
+    inline void E220::waitUntilCommandAccept()
+    {
+        if (AUXPin)
+        {
+            lastWaitUntilCommandAcceptMs = millis();
+
+            while (!digitalRead(*AUXPin))    // AUXPinがLOWの間はコマンドを受け付けない
+            {
+                delayMicroseconds(10);    // チャタリング防止
+
+                if (millis() - lastWaitUntilCommandAcceptMs > 200)
+                {
+                    break;    // タイムアウトした場合は強制的にコマンドを受け付ける
+                }
+            }
+            delay(3);    // mode切り替え指示後2~3ms秒後に切り替わる
+        }
+        else
+        {
+            delay(200);    // AUXPin が設定されていない場合は200ms待つ
+        }
+
+        // 待機している間に余計なデータが送られてきている可能性があるので読み捨てる
+        while (uart.available())
+        {
+            uart.read();
+        }
+    }
+}    // namespace Udon
--- src/Udon/Com/Lora/E220.hpp
+++ src/Udon/Com/Lora/E220.hpp
@@ -8,9 +8,49 @@
 
 namespace Udon
 {
+    //
+    //   E220 Loraモジュール
+    //
     class E220
         : public ILora
     {
+    public:
+        /// @brief コンストラクタ
+        /// @param uart モジュールと接続しているUARTインスタンス
+        /// @param M0Pin 
+        /// @param M1Pin 
+        E220(HardwareSerial& uart, uint8_t M0Pin, uint8_t M1Pin);
+
+        E220(HardwareSerial& uart, uint8_t M0Pin, uint8_t M1Pin, uint8_t AUXPin);
+
+        /// @brief 通信開始
+        /// @param channel チャンネル番号
+        /// @param myAddress 自身のアドレス
+        /// @param targetAddress 送信先のアドレス
+        bool begin(uint8_t channel, uint16_t myAddress, uint16_t targetAddress);
+
+        /// @brief 更新
+        void update();
+
+        /// @brief バスの状態を表示
+        void show() const;
+
+        /// @brief 通信中であるか
+        /// @return 通信中であるならtrue
+        operator bool() const override { return uart && not isTimeout(700); }
+
+        /// @brief 使用可能なチャンネルの範囲に値を収める
+        /// @param channel 収める前のチャンネル数
+        /// @return 収めた後のチャンネル数
+        static int ClampChannel(int channel) { return constrain(channel, 0, 30); }
+
+        /// @brief 送信ノードを登録
+        void joinTx(LoraNode& node) override { txNode = &node; }
+
+        /// @brief 受信ノードを登録
+        void joinRx(LoraNode& node) override { rxNode = &node; }
+
+    private:
         HardwareSerial& uart;
 
         uint8_t  channel       = 0;
@@ -25,68 +65,6 @@ namespace Udon
 
         uint32_t lastWaitUntilCommandAcceptMs = 0;
 
-    public:
-        E220(HardwareSerial& uart, uint8_t M0Pin, uint8_t M1Pin)
-            : uart(uart)
-            , M0Pin(M0Pin)
-            , M1Pin(M1Pin)
-            , AUXPin(Udon::nullopt)
-        {
-        }
-        E220(HardwareSerial& uart, uint8_t M0Pin, uint8_t M1Pin, uint8_t AUXPin)
-            : uart(uart)
-            , M0Pin(M0Pin)
-            , M1Pin(M1Pin)
-            , AUXPin(AUXPin)
-        {
-        }
-
-        /// @brief Loraが使用可能かどうか
-        /// @return Loraが使用可能ならtrue
-        operator bool() const override { return uart && not isTimeout(700); }
-
-        /// @brief 通信開始
-        /// @param channel チャンネル番号
-        /// @param myAddress 自身のアドレス
-        /// @param targetAddress 送信先のアドレス
-        bool begin(uint8_t channel, uint16_t myAddress, uint16_t targetAddress);
-
-        void update();
-
-        /// @brief バスの状態を表示
-        void show() const
-        {
-            if (operator bool())
-            {
-                Serial.print("E220: OK  ");
-            }
-            else
-            {
-                Serial.print("E220: NG  ");
-            }
-
-            switch (getTransmitMode())
-            {
-            case TransmitMode::Send: Serial.print("SendMode "); break;
-            case TransmitMode::Receive: Serial.print("ReceiveMode "); break;
-            case TransmitMode::TwoWay: Serial.print("TwoWayMode "); break;
-            case TransmitMode::Empty: Serial.print("Empty "); break;
-            }
-        }
-
-        /// @brief 送信ノードを登録
-        /// @param node
-        void joinTx(LoraNode& node) override { txNode = &node; }
-
-        /// @brief 受信ノードを登録
-        void joinRx(LoraNode& node) override { rxNode = &node; }
-
-        static int ClampChannel(int channel)
-        {
-            return constrain(channel, 0, 30);
-        }
-
-    private:
         enum class TransmitMode
         {
             Send,
@@ -96,337 +74,19 @@ namespace Udon
         };
 
         /// @brief 通信モードを取得する
-        TransmitMode getTransmitMode() const
-        {
-            if (txNode)
-            {
-                if (rxNode)
-                {
-                    return TransmitMode::TwoWay;
-                }
-                else
-                {
-                    return TransmitMode::Send;
-                }
-            }
-            else if (rxNode)
-            {
-                return TransmitMode::Receive;
-            }
-            else
-            {
-                return TransmitMode::Empty;
-            }
-        }
+        TransmitMode getTransmitMode() const;
 
-        bool sendUpdate()
-        {
-            // 送信休止時間分間隔をあける
-            if (millis() - txNode->transmitMs > 50)
-            {
-                uart.write((targetAddress >> 8) & 0xFF);    // 上位アドレス
-                uart.write((targetAddress >> 0) & 0xFF);    // 下位アドレス
-                uart.write(channel);
-                Udon::BitPack(txNode->data, txNode->data + txNode->size, [this](uint8_t data)
-                              { uart.write(data); });
-                txNode->transmitMs = millis();
-            }
-            else
-            {
-                // 送信間隔が短すぎる場合は送信しない
-                return false;
-            }
-            return true;
-        }
+        bool sendUpdate();
 
-        bool receiveUpdate()
-        {
-            // FrameSize = data
-            const int FrameSize = Udon::BitPackedSize(rxNode->size);
+        bool receiveUpdate();
 
-            // 受信バッファにデータがない場合は何もしない
-            if (uart.available() < FrameSize)
-            {
-                return false;
-            }
+        void twoWayUpdate();
 
-            while (uart.peek() >> 7 != 1)
-            {
-                uart.read();
-                if (uart.available() < FrameSize)
-                {
-                    return false;
-                }
-            }
+        bool isTimeout(uint32_t timeoutMs) const;
 
-            // データ読み込み
-            if (not Udon::BitUnpack(rxNode->data, rxNode->data + rxNode->size, [this]() -> uint8_t
-                                    { return uart.read(); }))
-            {
-                // 先頭バイトの  MSB が 1 でない場合はデータが壊れているので読み捨て
-                while (uart.available())
-                {
-                    uart.read();
-                }
-                return false;
-            }
-
-            rxNode->transmitMs = millis();
-            return true;
-        }
-
-        void twoWayUpdate()
-        {
-        }
-
-        bool isTimeout(uint32_t timeoutMs) const
-        {
-            switch (getTransmitMode())
-            {
-            case TransmitMode::Send:
-                return millis() - txNode->transmitMs > timeoutMs;
-            case TransmitMode::Receive:
-                return millis() - rxNode->transmitMs > timeoutMs;
-            case TransmitMode::TwoWay:
-                return millis() - txNode->transmitMs > timeoutMs || millis() - rxNode->transmitMs > timeoutMs;
-            case TransmitMode::Empty:
-                return false;
-            default:
-                return false;
-            }
-        }
-
-        void waitUntilCommandAccept()
-        {
-            if (AUXPin)
-            {
-                lastWaitUntilCommandAcceptMs = millis();
-
-                while (!digitalRead(*AUXPin))    // AUXPinがLOWの間はコマンドを受け付けない
-                {
-                    delayMicroseconds(10);    // チャタリング防止
-
-                    if (millis() - lastWaitUntilCommandAcceptMs > 200)
-                    {
-                        break;    // タイムアウトした場合は強制的にコマンドを受け付ける
-                    }
-                }
-                delay(3);    // mode切り替え指示後2~3ms秒後に切り替わる
-            }
-            else
-            {
-                delay(200);    // AUXPin が設定されていない場合は200ms待つ
-            }
-
-            // 待機している間に余計なデータが送られてきている可能性があるので読み捨てる
-            while (uart.available())
-            {
-                uart.read();
-            }
-        }
+        void waitUntilCommandAccept();
     };
 
-    inline bool E220::begin(uint8_t channel, uint16_t myAddress, uint16_t targetAddress)
-    {
-        this->channel       = channel;
-        this->targetAddress = targetAddress;
-
-        uart.begin(9600);    // パラメータセット時のビットレートは固定されている。
-
-        if (AUXPin)
-        {
-            pinMode(*AUXPin, INPUT);
-        }
-        pinMode(M0Pin, OUTPUT);
-        pinMode(M1Pin, OUTPUT);
-
-        uart.setTimeout(10);
-
-        waitUntilCommandAccept();
-        digitalWrite(M0Pin, HIGH);    // M0をHIGHに設定
-        digitalWrite(M1Pin, HIGH);    // M1をHIGHに設定
-                                      // configMode パラメータセットモード
-
-        waitUntilCommandAccept();
-        uart.write((uint8_t)0xC1);
-        uart.write((uint8_t)0x00);
-        uart.write((uint8_t)0x08);
-
-        delay(500);
-        uart.read();    // 0xC1
-        uart.read();    // 0x00
-        uart.read();    // 0x08
-
-        const uint8_t ADDH = (myAddress >> 8) & 0xFF;
-        const uint8_t ADDL = (myAddress >> 0) & 0xFF;
-
-        const uint8_t defADDH      = uart.read();    // ADDH
-        const uint8_t defADDL      = uart.read();    // ADDL
-        const uint8_t defRate      = uart.read();    // uartRate(7,6,5) & airRate(4,3,2,1,0)
-        const uint8_t defPower     = uart.read();    // subPacket(7,6) & RSSI noise(5) & notUsed(4,3,2) & SendPower(1,0)
-        const uint8_t defChannel   = uart.read();    // channel
-        const uint8_t defTransMode = uart.read();    // add RSSIByte(7) & transmittionMode(6) & notUsed(5,4,3) & worCycle(2,1,0)
-        uart.read();                                 // KeyHighByte
-        uart.read();                                 // KeyLowByte
-        uart.read();                                 // Version
-
-        const uint8_t uartRate  = defRate >> 5;
-        const uint8_t airRate   = defRate && 0b00011111;
-        const uint8_t subPacket = defPower >> 6;
-        const uint8_t RSSInoise = (defPower >> 5) && 0b00000001;
-        const uint8_t sendPower = defPower && 0b00000011;
-        const uint8_t RSSIByte  = defTransMode >> 7;
-        const uint8_t transMode = (defTransMode >> 6) && 0b00000001;
-        const uint8_t worCycle  = defTransMode && 0b00000111;
-        // 受信完了
-        // ＜"C0"とか"C3"＞＋<開始アドレス>＋＜長さ＞＋<パラメータ>
-        Serial.println("defaultParameters-----------------");
-        Serial.print("ADDH     :");
-        Serial.println(defADDH);
-        Serial.print("ADDL     :");
-        Serial.println(defADDL);
-        Serial.print("channel  :");
-        Serial.println(defChannel);
-        Serial.print("uartRate :");
-        Serial.println(uartRate);
-        Serial.print("airRate  :");
-        Serial.println(airRate);
-        Serial.print("subPacket:");
-        Serial.println(subPacket);
-        Serial.print("sendPower:");
-        Serial.println(sendPower);
-        Serial.print("RSSInoise:");
-        Serial.println(RSSInoise ? "add" : "not add");
-        Serial.print("add RSSIByte :");
-        Serial.println(RSSIByte ? "add" : "not add");
-        Serial.print("TransMode:");
-        Serial.println(transMode ? "FixedTransmissionMode" : "TransparentMode");
-        Serial.print("WorCycle:");
-        Serial.println(worCycle);
-        Serial.println("---------------------------------");
-
-        Serial.println("setParameters-----------------");
-
-        if (ADDH != defADDH)
-        {
-            waitUntilCommandAccept();
-            uart.write((uint8_t)0xC2);
-            uart.write((uint8_t)0x00);
-            uart.write((uint8_t)0x01);
-            uart.write(ADDH);
-            delay(200);
-            uart.read();    // 0xC1
-            uart.read();    // 0x00
-            uart.read();    // 0x01
-        }
-
-        if (ADDL != defADDL)
-        {
-            waitUntilCommandAccept();
-            uart.write((uint8_t)0xC2);
-            uart.write((uint8_t)0x01);
-            uart.write((uint8_t)0x01);
-            uart.write(ADDL);
-            delay(200);
-            uart.read();    // 0xC1
-            uart.read();    // 0x01
-            uart.read();    // 0x01
-        }
-
-        if (ADDH != defADDH or ADDL != defADDL)
-        {
-            Serial.print("Address :");
-            Serial.print(myAddress);
-        }
-
-        if (defRate != 0b11100010)
-        {
-            waitUntilCommandAccept();
-            uart.write((uint8_t)0xC2);
-            uart.write((uint8_t)0x02);
-            uart.write((uint8_t)0x01);
-            uart.write((uint8_t)0b11100010);
-            delay(200);
-            uart.read();    // 0xC1
-            uart.read();    // 0x02
-            uart.read();    // 0x01
-            Serial.print("Uart & AirRate:");
-            Serial.println(uart.read(), BIN);
-        }
-
-        if (defPower != 0b00000001)
-        {
-            waitUntilCommandAccept();
-            uart.write((uint8_t)0xC2);
-            uart.write((uint8_t)0x03);
-            uart.write((uint8_t)0x01);
-            uart.write((uint8_t)0b00000001);
-            delay(200);
-            uart.read();    // 0xC1
-            uart.read();    // 0x03
-            uart.read();    // 0x01
-            Serial.print("SubPacket & RSSINoise & SendPower:");
-            Serial.println(uart.read(), BIN);
-        }
-
-        if (channel != defChannel)
-        {
-            waitUntilCommandAccept();
-            uart.write((uint8_t)0xC2);
-            uart.write((uint8_t)0x04);
-            uart.write((uint8_t)0x01);
-            uart.write(channel);
-            delay(200);
-            uart.read();    // 0xC1
-            uart.read();    // 0x02
-            uart.read();    // 0x01
-            Serial.print("Channel :");
-            Serial.println(uart.read());
-        }
-
-        if (transMode != 0b01000011)
-        {
-            waitUntilCommandAccept();
-            uart.write((uint8_t)0xC2);
-            uart.write((uint8_t)0x05);
-            uart.write((uint8_t)0x01);
-            uart.write((uint8_t)0b01000011);
-            delay(200);
-            uart.read();    // 0xC1
-            uart.read();    // 0x05
-            uart.read();    // 0x01
-            Serial.print("RSSIByte & TransMode & WORCycle:");
-            Serial.println(uart.read(), BIN);
-        }
-        Serial.println("---------------------------------");
-
-        // 設定モードの終了
-        waitUntilCommandAccept();
-        digitalWrite(M0Pin, LOW);
-        digitalWrite(M1Pin, LOW);
-
-        waitUntilCommandAccept();
-        uart.end();
-        uart.begin(115200);    // 通常通信モードのレートに変更する
-        uart.setTimeout(10);
-        return true;
-    }
-
-    inline void E220::update()
-    {
-        switch (getTransmitMode())
-        {
-        case TransmitMode::Send:
-            sendUpdate();
-            break;
-        case TransmitMode::Receive:
-            receiveUpdate();
-            break;
-        case TransmitMode::TwoWay:
-            twoWayUpdate();
-            break;
-        case TransmitMode::Empty:
-            break;
-        }
-    }
 }    // namespace Udon
+
+#include "Detail/E220.hpp"
