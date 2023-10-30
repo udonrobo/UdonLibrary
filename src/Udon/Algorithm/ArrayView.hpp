d3f5ef7d55c77fea64a788b9a613a35667af308e
CaseyNelson <caseynelson314@gmail.com>
2023-10-31 01:20:04 +0900

    ビューを加工する関数を複数定義

--- src/Udon/Algorithm/ArrayView.hpp
+++ src/Udon/Algorithm/ArrayView.hpp
@@ -16,6 +16,7 @@
 #pragma once
 
 #include <Udon/Stl/EnableSTL.hpp>
+#include <Udon/Common/Platform.hpp>
 #include <cstddef>    // std::ptrdiff_t
 #include <iterator>
 
@@ -26,6 +27,7 @@ namespace Udon
     class ArrayView
     {
     public:
+        using size_type              = size_t;
         using value_type             = T;
         using reference              = T&;
         using const_reference        = const T&;
@@ -37,8 +39,8 @@ namespace Udon
         using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
     private:
-        pointer m_data;
-        size_t  m_size;
+        pointer   m_data;
+        size_type m_size;
 
     public:
         ArrayView()
@@ -47,14 +49,20 @@ namespace Udon
         {
         }
 
-        template <size_t N>
+        template <size_type N>
         ArrayView(value_type (&m_data)[N])
             : m_data(m_data)
             , m_size(N)
         {
         }
 
-        ArrayView(pointer m_data, const size_t m_size)
+        ArrayView(const char* string)
+            : m_data(string)
+            , m_size(strlen(string))
+        {
+        }
+
+        ArrayView(pointer m_data, const size_type m_size)
             : m_data(m_data)
             , m_size(m_size)
         {
@@ -72,7 +80,7 @@ namespace Udon
             return m_size;
         }
 
-        constexpr size_t size() const
+        constexpr size_type size() const
         {
             return m_size;
         }
@@ -86,20 +94,20 @@ namespace Udon
             return m_data;
         }
 
-        reference operator[](const size_t index)
+        reference operator[](const size_type index)
         {
             return m_data[index];
         }
-        const_reference operator[](const size_t index) const
+        const_reference operator[](const size_type index) const
         {
             return m_data[index];
         }
 
-        reference at(const size_t index)
+        reference at(const size_type index)
         {
             return m_data[index];
         }
-        const_reference at(const size_t index) const
+        const_reference at(const size_type index) const
         {
             return m_data[index];
         }
@@ -140,6 +148,75 @@ namespace Udon
             }
         }
 
+        /// @brief 指定された範囲からビューを作成する。
+        /// @param beginIndex 開始位置
+        /// @param endIndex 終端位置
+        /// @return 
+        ArrayView subView(size_type beginIndex, size_type endIndex) const
+        {
+            return {
+                std::next(cbegin(), beginIndex),
+                std::min(endIndex - beginIndex, m_size)
+            };
+        }
+
+        /// @brief 指定された範囲からビューを作成する。
+        /// @remark 終端は現在のビューの終端
+        /// @param beginIndex 開始位置
+        /// @return 
+        ArrayView subView(size_type beginIndex) const
+        {
+            return {
+                std::next(cbegin(), beginIndex),
+                m_size
+            };
+        }
+
+        /// @brief 特定の値まで検索し、そこまでの範囲の一つ手前までをビューとする。
+        /// @param terminate 終端の値
+        /// @return 
+        ArrayView subViewUntil(const value_type& terminate) const
+        {
+            return {
+                cbegin(),
+                std::find(cbegin(), cend(), terminate)
+            };
+        }
+
+        /// @brief 自身が指定のビューから始まるかどうか
+        /// @remark 
+        ///      [0, 1, 2, 3, 4].startsWith([0, 1, 2]) == true
+        ///      [4, 3, 2, 1, 0].startsWith([0, 1, 2]) == false
+        /// @param rhs 
+        /// @return 
+        bool startsWith(const ArrayView& rhs) const
+        {
+            if (size() >= rhs.size())
+            {
+                return std::equal(rhs.cbegin(), rhs.cend(), cbegin());
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        friend bool operator==(const ArrayView& lhs, const ArrayView& rhs)
+        {
+            if (lhs.size() == rhs.size())
+            {
+                return std::equal(lhs.cbegin(), lhs.cend(), rhs.cbegin());
+            }
+            else
+            {
+                return false;
+            }
+        }
+        friend bool operator!=(const ArrayView& lhs, const ArrayView& rhs)
+        {
+            return not(lhs == rhs);
+        }
+
         iterator begin()
         {
             return { m_data };
@@ -148,7 +225,7 @@ namespace Udon
         {
             return { m_data };
         }
-        
+
         iterator end()
         {
             return { m_data + m_size };
@@ -190,11 +267,47 @@ namespace Udon
         {
             return const_reverse_iterator{ cend() };
         }
-        
+
         const_reverse_iterator crend() const
         {
             return const_reverse_iterator{ cbegin() };
         }
+
+#if UDON_PLATFORM_OUTPUT_STREAM == UDON_PLATFORM_OUTPUT_CONSOLE
+        friend std::ostream& operator<<(std::ostream& ostm, const ArrayView& rhs)
+        {
+            bool isFirst = true;
+            for (auto&& element : rhs)
+            {
+                if (isFirst)
+                {
+                    ostm << element;
+                }
+                else
+                {
+                    ostm << ", " << element;
+                }
+                isFirst = false;
+            }
+            return ostm;
+        }
+#endif
+
+#ifdef ARDUINO
+        void show() const
+        {
+            Serial.print("[");
+            for (size_type i = 0; i < m_size; ++i)
+            {
+                Serial.print(m_data[i]);
+                if (i < m_size - 1)
+                {
+                    Serial.print(", ");
+                }
+            }
+            Serial.println("]");
+        }
+#endif
     };
 
 }    // namespace Udon
