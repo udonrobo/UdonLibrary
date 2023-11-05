aed4571819f7c01066c9984b7f4d4817434d251b
CaseyNelson <caseynelson314@gmail.com>
2023-11-06 01:36:58 +0900

    ボタンクラスと入力クラスの分離をやめた

--- src/Udon/Algorithm/Button.hpp
+++ src/Udon/Algorithm/Button.hpp
@@ -17,16 +17,59 @@
 
 namespace Udon
 {
-    struct Button
+
+    /// @brief 入力値
+    class Input
     {
 
+        /// @brief 過去値
+        bool previous = false;
+
+    public:
         /// @brief 押されているか
-        bool press;
+        bool press = false;
 
         /// @brief 押された瞬間か
-        bool click;
+        bool click = false;
 
         /// @brief 離された瞬間か
-        bool release;
+        bool release = false;
+
+        /// @brief トグル値
+        bool toggle = false;
+
+        //                   │
+        //   [press:true ]   │            ┌─────────────┐             ┌─────────────┐             ┌──
+        //                   │            │             │             │             │             │
+        //   [press:false]   ┼────────────┘             └─────────────┘             └─────────────┘
+        //                   │
+        //
+        //                   │
+        //   [click:true ]   │            ┌┐                          ┌┐                          ┌┐
+        //                   │            ││                          ││                          ││
+        //   [click:false]   ┼────────────┘└──────────────────────────┘└──────────────────────────┘└─
+        //                   │
+        //
+        //                   │
+        //   [release:true ] │                          ┌┐                          ┌┐
+        //                   │                          ││                          ││
+        //   [release:false] ┼──────────────────────────┘└──────────────────────────┘└───────────────
+        //                   │
+        //
+        //                   │
+        //   [toggle:true ]  │            ┌───────────────────────────┐                           ┌──
+        //                   │            │                           │                           │
+        //   [toggle:false]  ┼────────────┘                           └───────────────────────────┘
+        //                   │
+
+        /// @brief 入力値を更新する
+        /// @param value 新規入力値
+        void update(bool value)
+        {
+            press   = value;
+            click   = not previous and value;
+            release = previous and not value;
+            toggle ^= click;
+        }
     };
 }    // namespace Udon
