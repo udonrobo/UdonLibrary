a1f366c0023cdf33429ae2e7e9807ac256b8a69c
CaseyNelson <caseynelson314@gmail.com>
2023-12-18 22:05:46 +0900

    fixed

--- src/Udon/Com/Pad.hpp
+++ src/Udon/Com/Pad.hpp
@@ -1,14 +1,29 @@
 #pragma once
 
-#include "Pad/PadPS5.hpp"
+// Arduino
+#ifdef ARDUINO
 
-#include "Can.hpp"
-#include "I2c.hpp"
-#include "Uart.hpp"
+#    include "Can.hpp"
+#    include "I2c.hpp"
+#    include "Uart.hpp"
+#    include "Pad/PadPS5.hpp"
 
 namespace Udon
 {
-    using CanPadPS5 = PadPS5<CanReader>;
-    using I2cPadPS5 = PadPS5<I2cMasterReader>;
+    using CanPadPS5  = PadPS5<CanReader>;
+    using I2cPadPS5  = PadPS5<I2cMasterReader>;
     using UartPadPS5 = PadPS5<UartReader>;
-}
+}    // namespace Udon
+#endif
+
+// OpenSiv3D
+#ifdef SIV3D_INCLUDED
+
+#    include "Pad/SivPadPS5.hpp"
+
+namespace Udon
+{
+    using SivPadPS5 = PadPS5<SivPadPS5Reader>;
+}    // namespace Udon
+
+#endif
