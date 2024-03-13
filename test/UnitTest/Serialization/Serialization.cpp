d4698f2417640e372e334d577478d6528d63203c
CaseyNelson <caseynelson314@gmail.com>
2024-03-13 18:26:36 +0900

    シリアライザのテストを修正

--- test/UnitTest/Serialization/Serialization.cpp
+++ test/UnitTest/Serialization/Serialization.cpp
@@ -1,49 +1,56 @@
 #include <gtest/gtest.h>
-
+#include <Udon/Types/Vector2D.hpp>
 #include <Udon/Serializer/Serializer.hpp>
 
-struct Vec2
+TEST(Serialization, SerializedSize)
 {
-    double x, y;
-    bool operator==(const Vec2& rhs) const { return x == rhs.x && y == rhs.y; }
-    bool operator!=(const Vec2& rhs) const { return !(*this == rhs); }
-    UDON_ENUMERABLE(x, y);
-};
+    EXPECT_TRUE(Udon::SerializedSize<bool>() == sizeof(bool) + Udon::CRC8_SIZE);
 
-TEST(Serialization, BufferSizeAfterSerialization)
-{
-    EXPECT_EQ(Udon::SerializedSize<bool    >(), 1     + 1);
-    EXPECT_EQ(Udon::SerializedSize<uint8_t >(), 1     + 1);
-    EXPECT_EQ(Udon::SerializedSize<uint16_t>(), 2     + 1);
-    EXPECT_EQ(Udon::SerializedSize<uint32_t>(), 4     + 1);
-    EXPECT_EQ(Udon::SerializedSize<uint64_t>(), 8     + 1);
-    EXPECT_EQ(Udon::SerializedSize<float   >(), 4     + 1);
-    EXPECT_EQ(Udon::SerializedSize<double  >(), 4     + 1);
-    EXPECT_EQ(Udon::SerializedSize<Vec2    >(), 4 + 4 + 1);
+    EXPECT_TRUE(Udon::SerializedSize<uint8_t>() == sizeof(uint8_t) + Udon::CRC8_SIZE);
+
+    EXPECT_TRUE(Udon::SerializedSize<uint16_t>() == sizeof(uint16_t) + Udon::CRC8_SIZE);
+
+    EXPECT_TRUE(Udon::SerializedSize<uint32_t>() == sizeof(uint32_t) + Udon::CRC8_SIZE);
+
+    EXPECT_TRUE(Udon::SerializedSize<uint64_t>() == sizeof(uint64_t) + Udon::CRC8_SIZE);
+
+    EXPECT_TRUE(Udon::SerializedSize<float>() == sizeof(float) + Udon::CRC8_SIZE);
+
+    EXPECT_TRUE(Udon::SerializedSize<double>() == sizeof(double) + Udon::CRC8_SIZE);
+
+    EXPECT_TRUE(Udon::SerializedSize<Udon::Vec2>() == sizeof(Udon::Vec2::ValueType) * Udon::Vec2::Dimension + Udon::CRC8_SIZE);
 }
 
-TEST(Serialization, SerializeDeserialize)
+TEST(Serialization, Serialize)
 {
-	Vec2 v{ 1.0, 2.0 };
+    Udon::Vec2 v{ 1.0, 2.0 };
 
-    const auto serialized = Udon::Serialize(v);
-    EXPECT_EQ(Udon::SerializedSize<Vec2>(), serialized.size());    // シリアライズ後のサイズが正しいか確認
-    EXPECT_EQ(serialized.back(), Udon::CRC8({ serialized.data(), serialized.size() - 1 }));    // CRCが正しいか確認
+    auto serialized = Udon::Serialize(v);
 
-    const auto deserialized = Udon::Deserialize<Vec2>(serialized);
+    EXPECT_TRUE(Udon::SerializedSize<Udon::Vec2>() == serialized.size());                          // シリアライズ後のサイズが正しいか確認
+    EXPECT_TRUE(serialized.back() == Udon::CRC8({ serialized.data(), serialized.size() - 1 }));    // CRCが正しいか確認
+}
+
+TEST(Serialization, Deserialize)
+{
+    Udon::Vec2 v{ 1.0, 2.0 };
+
+    auto deserialized = Udon::Deserialize<Udon::Vec2>(Udon::Serialize(v));
+
     EXPECT_TRUE(deserialized.has_value());    // デシリアライズに成功したか確認
-    EXPECT_EQ(v, deserialized.value());       // デシリアライズ後の値が正しいか確認
+    EXPECT_TRUE(v == deserialized.value());       // デシリアライズ後の値が正しいか確認
 }
 
 TEST(Serialization, DeserializeError)
 {
-    Vec2 v{ 1.0, 2.0 };
+    Udon::Vec2 v{ 1.0, 2.0 };
+
     auto serialized = Udon::Serialize(v);
 
-    serialized[2] = 0x00;  // CRCが不正になるように変更
+    serialized[1] = 0x00;    // CRCが不正になるように変更
 
-    const auto deserialized = Udon::Deserialize<Vec2>(serialized);
+    auto deserialized = Udon::Deserialize<Udon::Vec2>(serialized);
 
-    EXPECT_FALSE(deserialized.has_value());
-    EXPECT_NE(v, deserialized.value());
+    EXPECT_TRUE(not deserialized.has_value());
+    EXPECT_TRUE(v != deserialized.value());
 }
