ba315b90f6202b7d2fe062f7c94aaa3ce608e307
CaseyNelson <caseynelson314@gmail.com>
2024-03-14 23:43:21 +0900

    リンク切れ修正

--- src/Udon/Traits/EnumerableMacro.hpp
+++ src/Udon/Traits/EnumerableMacro.hpp
@@ -1,11 +1,10 @@
-#pragma once    
+#pragma once
 
 /// @brief メンバ変数の列挙を可能にする
 /// @param ... 列挙可能なメンバー変数(','区切り)
-#define UDON_ENUMERABLE(...)                                                                  \
+#define UDON_ENUMERABLE(...)                                                                \
     template <typename Enumerator>                                                          \
     constexpr typename Enumerator::ResultType enumerate(const Enumerator& enumerator) const \
     {                                                                                       \
         return enumerator(__VA_ARGS__);                                                     \
     }
-
