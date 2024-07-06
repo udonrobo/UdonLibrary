3ddb8b4ddc44d548275330d7f36e80d351af3cc7
CaseyNelson <caseynelson314@gmail.com>
2024-07-06 22:04:23 +0900

    デバイスドライバーのテストコード追記

---
+++ test/ArduinoLint/src/Driver/SegmentsLed.cpp
@@ -0,0 +1,21 @@
+//
+//    構文テスト
+//
+//    Copyright (c) 2022-2024 udonrobo
+//
+
+#include <Arduino.h>
+#include <Udon/Driver/SegmentsLed.hpp>
+
+inline void test()
+{
+    Udon::SegmentsLed led{
+        { 2, 3, 4, 5, 6, 7, 8 },
+        { 10, 11, 12, 13 },
+    };
+
+    led.begin();
+    led.setValue(1234);
+    led.show();
+    led.showNan();
+}
