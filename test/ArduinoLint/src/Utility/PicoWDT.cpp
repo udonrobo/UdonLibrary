3f143eaadc7e0876b9e53b3fdb2888b5ba8fb348
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2024-07-10 18:02:01 +0900

    テスト追加

---
+++ test/ArduinoLint/src/Utility/PicoWDT.cpp
@@ -0,0 +1,18 @@
+//
+//    Raspberry Pi Pico 用ウォッチドックタイマー
+//
+//    Copyright (c) 2022-2024 udonrobo
+//
+
+#ifdef ARDUINO_ARCH_RP2040
+
+#include <Arduino.h>
+#include <Udon/Utility/PicoWDT.hpp>
+
+inline void test()
+{
+    Udon::PicoWDT wdt;
+    wdt.update();
+}
+
+#endif
