f23893a48981aedf902065b71066a0975ef27d77
po-po-po-po-po-po <riku.seto2007@gmail.com>
2024-07-10 23:06:42 +0900

    begin, updateのみに制限しました。

--- src/Udon/Algorithm/FunctionCallTracker.hpp
+++ src/Udon/Algorithm/FunctionCallTracker.hpp
@@ -3,11 +3,43 @@
 #include <Udon/Common/Show.hpp>
 #include <Udon/Stl/EnableSTL.hpp>
 #include <utility>
+#include <type_traits>
 #include <map>
 #include <string>
 
 namespace Udon
 {
+    /// @brief begin, update が呼び出されているか確認する
+    template <typename T>
+    class callTracker
+    {
+    private:
+        bool beginCalled;
+        bool updateCalled;
+
+    public:
+        callTracker()
+            : beginCalled(false)
+            , updateCalled(false)
+        {
+        }
+
+        void callBegin() { beginCalled = true; }
+        void callUpdate() { updateCalled = true; }
+
+        void check()
+        {
+            if (!beginCalled && Udon::Traits::HasMemberFunctionBegin<T>::value)
+            {
+                Udon::Showln("warning: begin  is not called!");
+            }
+            if (!updateCalled && Udon::Traits::HasMemberFunctionUpdate<T>::value)
+            {
+                Udon::Showln("warning: update is not called!");
+            }
+        }
+    };
+
 
     /// @brief 指定した関数が呼び出されているか確認する
     template <typename T>
@@ -31,9 +63,17 @@ namespace Udon
 
         /// @brief 監視したい関数を登録する
         /// @note begin , update は自動的に登録されます。
-        void registration(const std::string& functionName)
+        template <class Head, class... Tail>
+        void registration(const Head& functionName, const Tail&... tail)
         {
-            functions[functionName] = false;
+            static_assert(std::is_convertible<Head, std::string>::value, "Argument must be convertible to std::string");
+            functions[std::string(functionName)] = false;
+            registration(tail...);
+        }
+
+        // 終了条件
+        void registration()
+        {
         }
 
         /// @brief 関数が呼ばれたことを報告する
@@ -74,7 +114,8 @@ namespace Udon
                 if (!function.second)
                 {
                     Udon::Showln(function.first.c_str(), "not called");
-                }else
+                }
+                else
                 {
                     Udon::Showln(function.first.c_str(), "          ", "  called");
                 }
