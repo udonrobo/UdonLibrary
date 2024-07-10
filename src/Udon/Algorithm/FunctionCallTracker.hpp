d8d7891aba67af3022a23b843a18a42bd0cd7616
po-po-po-po-po-po <riku.seto2007@gmail.com>
2024-07-10 17:22:47 +0900

    動いた(変更する予定)

--- src/Udon/Algorithm/FunctionCallTracker.hpp
+++ src/Udon/Algorithm/FunctionCallTracker.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <Udon/Common/Show.hpp>
 #include <Udon/Stl/EnableSTL.hpp>
-#include <array>
 #include <utility>
+#include <map>
 #include <string>
 
 namespace Udon
@@ -16,32 +17,68 @@ namespace Udon
         std::map<std::string, bool> functions;
 
     public:
-        FunctionCallTracker() {}
+        FunctionCallTracker()
+        {
+            if (Udon::Traits::HasMemberFunctionBegin<T>::value)
+            {
+                functions["begin"] = false;
+            }
+            if (Udon::Traits::HasMemberFunctionUpdate<T>::value)
+            {
+                functions["update"] = false;
+            }
+        }
 
         /// @brief 監視したい関数を登録する
+        /// @note begin , update は自動的に登録されます。
         void registration(const std::string& functionName)
         {
             functions[functionName] = false;
         }
 
-        /// @brief 監視している関数が呼ばれたことを報告する
+        /// @brief 関数が呼ばれたことを報告する
         void call(const std::string& functionName)
         {
             functions[functionName] = true;
         }
 
+        /// @brief 監視している関数の登録を解除する
+        void deregister(const std::string& functionName)
+        {
+            functions.erase(functionName);
+        }
+
+        /// @brief 関数の呼び出しをリセットする
+        void rejected(const std::string& functionName)
+        {
+            functions[functionName] = false;
+        }
+
         /// @brief 監視している関数が呼ばれていない場合、警告を表示する
-        /// @return 登録されている関数の状況
-        auto check()
+        void check()
         {
             for (const auto& function : functions)
             {
                 if (!function.second)
                 {
-                    Udon::Showln("warning :", function.first, "is not called!");
+                    Udon::Showln("warning :", function.first.c_str(), "is not called!");
                 }
             }
-            return functions;
+        }
+
+        /// @brief 監視している全ての関数の呼び出し状況を表示する
+        void showAll()
+        {
+            for (const auto& function : functions)
+            {
+                if (!function.second)
+                {
+                    Udon::Showln(function.first.c_str(), "not called");
+                }else
+                {
+                    Udon::Showln(function.first.c_str(), "          ", "  called");
+                }
+            }
         }
     };
 }    // namespace Udon
