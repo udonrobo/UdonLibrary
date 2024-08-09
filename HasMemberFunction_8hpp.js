f483d1c15c373caeaccdc78ae4f42d391e549e86
CaseyNelson314 <CaseyNelson314@users.noreply.github.com>
2024-08-09 07:20:05 +0000

    deploy: d128c7c3b8ca956cc7e236073db024eecc50057f

--- HasMemberFunction_8hpp.js
+++ HasMemberFunction_8hpp.js
@@ -1,13 +1,13 @@
 var HasMemberFunction_8hpp =
 [
     [ "Udon::Traits::HasMemberFunctionBegin< typename, typename >", "structUdon_1_1Traits_1_1HasMemberFunctionBegin.html", null ],
-    [ "Udon::Traits::HasMemberFunctionBegin< T, std::void_t< decltype(std::declval< T >().begin())> >", "structUdon_1_1Traits_1_1HasMemberFunctionBegin_3_01T_00_01std_1_1void__t_3_01decltype_07std_1_1d477eafb4e1e426f26cfdd9d1986528d6.html", null ],
+    [ "Udon::Traits::HasMemberFunctionBegin< T, std::void_t< decltype(&T::begin)> >", "structUdon_1_1Traits_1_1HasMemberFunctionBegin_3_01T_00_01std_1_1void__t_3_01decltype_07_6T_1_1begin_08_4_01_4.html", null ],
     [ "Udon::Traits::HasMemberFunctionShow< typename, typename >", "structUdon_1_1Traits_1_1HasMemberFunctionShow.html", null ],
-    [ "Udon::Traits::HasMemberFunctionShow< T, std::void_t< decltype(std::declval< T >().show())> >", "structUdon_1_1Traits_1_1HasMemberFunctionShow_3_01T_00_01std_1_1void__t_3_01decltype_07std_1_1dea70867b32c031791c9e87d10afb768d8.html", null ],
+    [ "Udon::Traits::HasMemberFunctionShow< T, std::void_t< decltype(&T::show)> >", "structUdon_1_1Traits_1_1HasMemberFunctionShow_3_01T_00_01std_1_1void__t_3_01decltype_07_6T_1_1show_08_4_01_4.html", null ],
     [ "Udon::Traits::HasMemberFunctionShowRaw< typename, typename >", "structUdon_1_1Traits_1_1HasMemberFunctionShowRaw.html", null ],
-    [ "Udon::Traits::HasMemberFunctionShowRaw< T, std::void_t< decltype(std::declval< T >().showRaw())> >", "structUdon_1_1Traits_1_1HasMemberFunctionShowRaw_3_01T_00_01std_1_1void__t_3_01decltype_07std_1_3e444e507562d9b8f48e0eedd1a2ed7c.html", null ],
+    [ "Udon::Traits::HasMemberFunctionShowRaw< T, std::void_t< decltype(&T::showRaw)> >", "structUdon_1_1Traits_1_1HasMemberFunctionShowRaw_3_01T_00_01std_1_1void__t_3_01decltype_07_6T_1_1showRaw_08_4_01_4.html", null ],
     [ "Udon::Traits::HasMemberFunctionUpdate< typename, typename >", "structUdon_1_1Traits_1_1HasMemberFunctionUpdate.html", null ],
-    [ "Udon::Traits::HasMemberFunctionUpdate< T, std::void_t< decltype(std::declval< T >().update())> >", "structUdon_1_1Traits_1_1HasMemberFunctionUpdate_3_01T_00_01std_1_1void__t_3_01decltype_07std_1_15de3e403ae1414ee66580615516c8a49.html", null ],
+    [ "Udon::Traits::HasMemberFunctionUpdate< T, std::void_t< decltype(&T::update)> >", "structUdon_1_1Traits_1_1HasMemberFunctionUpdate_3_01T_00_01std_1_1void__t_3_01decltype_07_6T_1_1update_08_4_01_4.html", null ],
     [ "Udon::Traits::DummyEnumerator", "structUdon_1_1Traits_1_1DummyEnumerator.html", "structUdon_1_1Traits_1_1DummyEnumerator" ],
     [ "Udon::Traits::HasMemberFunctionEnumerate< T, typename >", "structUdon_1_1Traits_1_1HasMemberFunctionEnumerate.html", null ],
     [ "Udon::Traits::HasMemberFunctionEnumerate< T, typename std::enable_if< std::is_same< decltype(std::declval< T >().enumerate(std::declval< DummyEnumerator & >())), typename DummyEnumerator::ResultType >::value >::type >", "structUdon_1_1Traits_1_1HasMemberFunctionEnumerate_3_01T_00_01typename_01std_1_1enable__if_3_01sff0f025c1d5b2a39133ed2b0fcaf15dc.html", null ],
