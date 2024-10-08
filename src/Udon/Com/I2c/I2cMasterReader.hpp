3b01541e1f933fcc6225c256259cf9ec5fa24e5c
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2024-10-08 09:35:10 +0900

    区切り文字の指定はできないため、変に指定していた個所を修正

--- src/Udon/Com/I2c/I2cMasterReader.hpp
+++ src/Udon/Com/I2c/I2cMasterReader.hpp
@@ -69,12 +69,11 @@ namespace Udon
         }
 
         /// @brief 受信内容を表示
-        /// @param gap 区切り文字 (default: '\t')
-        void show(char gap = '\t') const
+        void show() const
         {
             if (const auto message = getMessage())
             {
-                Udon::Show(*message, gap);
+                Udon::Show(*message);
             }
             else
             {
@@ -83,10 +82,9 @@ namespace Udon
         }
 
         /// @brief 受信バッファを表示
-        /// @param gap 区切り文字 (default: ' ')
-        void showRaw(char gap = ' ') const
+        void showRaw() const
         {
-            Udon::Show(buffer, gap);
+            Udon::Show(buffer);
         }
 
     private:
