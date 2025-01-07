79b67dc2f51282426a72f778474f6a54336d69fc
CaseyNelson314 <CaseyNelson314@users.noreply.github.com>
2025-01-07 08:27:42 +0000

    deploy: 096c7b783e935375b42adf147fa8d94b0008b477

--- classUdon_1_1RoboMasterC620.js
+++ classUdon_1_1RoboMasterC620.js
@@ -4,5 +4,5 @@ var classUdon_1_1RoboMasterC620 =
     [ "RoboMasterBase", "classUdon_1_1RoboMasterC620.html#a30705bfd2bb86c333a9cd74371915de6", null ],
     [ "RoboMasterBase", "classUdon_1_1RoboMasterC620.html#ad596fdad7d884dd3aa163ef7338eb4bc", null ],
     [ "RoboMasterBase", "classUdon_1_1RoboMasterC620.html#a7a04ac61f4f4a61f24d8f387af1c542e", null ],
-    [ "setCurrent", "classUdon_1_1RoboMasterC620.html#a041ca908cd740ffb8ff19ada9415c1dc", null ]
+    [ "setCurrent", "classUdon_1_1RoboMasterC620.html#a27342ba524104eb5c2158d247c6eecb8", null ]
 ];
