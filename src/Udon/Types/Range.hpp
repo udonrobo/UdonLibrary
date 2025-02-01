6a1bbb03ef22d4fb18008d95e180382270fd485a
Okawa Yusuke <caseynelson314@gmail.com>
2025-02-01 21:11:03 +0900

    コメント追記

--- src/Udon/Types/Range.hpp
+++ src/Udon/Types/Range.hpp
@@ -2,10 +2,11 @@
 
 namespace Udon
 {
+    /// @brief 範囲を表す型
     template <typename MinT, typename MaxT = MinT>
     struct Range
     {
-        MinT min;
-        MaxT max;
+        MinT min;  ///< 最小値
+        MaxT max;  ///< 最大値
     };
 }    // namespace Udon
